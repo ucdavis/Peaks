49cdf32f8dcec1c06ba45f370271236adee1289d
Scott Kirkland <srkirkland@gmail.com>
2017-09-14 16:38:51 -0700

    login and show username

---
+++ Keas.Mvc/Extensions/ClaimsExtensions.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Security.Claims;
+using System.Threading.Tasks;
+
+namespace Keas.Mvc.Extensions
+{
+    public static class ClaimsExtensions
+    {
+        public static string GetNameClaim(this ClaimsPrincipal claimsPrincipal)
+        {
+            if (claimsPrincipal == null || !claimsPrincipal.Identity.IsAuthenticated) return string.Empty;
+
+            var nameClaim = claimsPrincipal.FindFirst("name");
+
+            return nameClaim != null ? nameClaim.Value : claimsPrincipal.Identity.Name;
+        }
+    }
+}
