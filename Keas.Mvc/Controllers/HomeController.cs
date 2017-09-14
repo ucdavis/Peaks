b423371255d9d644e800614eb11b29e0d0755c0b
Scott Kirkland <srkirkland@gmail.com>
2017-09-14 16:28:57 -0700

    setup authentication

--- Keas.Mvc/Controllers/HomeController.cs
+++ Keas.Mvc/Controllers/HomeController.cs
@@ -5,9 +5,11 @@ using System.Linq;
 using System.Threading.Tasks;
 using Microsoft.AspNetCore.Mvc;
 using Keas.Mvc.Models;
+using Microsoft.AspNetCore.Authorization;
 
 namespace Keas.Mvc.Controllers
 {
+    [Authorize]
     public class HomeController : Controller
     {
         public IActionResult Index()
--- Keas.Mvc/Startup.cs
+++ Keas.Mvc/Startup.cs
@@ -2,6 +2,8 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
+using Microsoft.AspNetCore.Authentication.Cookies;
+using Microsoft.AspNetCore.Authentication.OpenIdConnect;
 using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
 using Microsoft.Extensions.Configuration;
@@ -21,6 +23,29 @@ namespace Keas.Mvc
         // This method gets called by the runtime. Use this method to add services to the container.
         public void ConfigureServices(IServiceCollection services)
         {
+            // add openID connect auth backed by a cookie signin scheme
+            services.AddAuthentication(options =>
+            {
+                options.DefaultChallengeScheme = OpenIdConnectDefaults.AuthenticationScheme;
+                options.DefaultSignInScheme = OpenIdConnectDefaults.AuthenticationScheme;
+                options.DefaultAuthenticateScheme = OpenIdConnectDefaults.AuthenticationScheme;
+            })
+            .AddCookie()
+            .AddOpenIdConnect(options =>
+            {
+                options.SignInScheme = CookieAuthenticationDefaults.AuthenticationScheme;
+                options.GetClaimsFromUserInfoEndpoint = true;
+                options.ClientId = "d80bb7cd-b059-4230-9a33-a53f86fab0c4";
+                options.Authority = "https://login.microsoftonline.com/ucdavis365.onmicrosoft.com";
+                options.Events.OnRedirectToIdentityProvider = context =>
+                {
+                    // this allows us to go straight to the UCD login
+                    context.ProtocolMessage.SetParameter("domain_hint", "ucdavis.edu");
+
+                    return Task.FromResult(0);
+                };
+            });
+
             services.AddMvc();
         }
 
@@ -37,6 +62,8 @@ namespace Keas.Mvc
             }
 
             app.UseStaticFiles();
+
+            app.UseAuthentication();
 
             app.UseMvc(routes =>
             {
