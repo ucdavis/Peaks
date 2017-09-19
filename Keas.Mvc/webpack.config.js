c3066216397d5e891155b59df3f3c6a83a9e3b52
John Knoll <jpknoll@gmail.com>
2017-09-19 10:54:00 -0700

    working vendor

--- Keas.Mvc/webpack.config.js
+++ Keas.Mvc/webpack.config.js
@@ -29,7 +29,7 @@ module.exports = (env) => {
             new CheckerPlugin(),
             new webpack.optimize.CommonsChunkPlugin({
                 name: 'vendor',
-                chunks: ['boot']
+                minChunks: Infinity,
             })
 
         ].concat(isDevBuild ? [
