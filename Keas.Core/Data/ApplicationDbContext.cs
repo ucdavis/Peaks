73dd6cc6e54062a6b784e6db54a9d4b5ac439cc0
Scott Kirkland <srkirkland@gmail.com>
2017-09-19 11:50:21 -0700

    Add in reference to db context

--- Keas.Core/Data/ApplicationDbContext.cs
+++ Keas.Core/Data/ApplicationDbContext.cs
@@ -3,8 +3,12 @@ using Microsoft.EntityFrameworkCore;
 
 namespace Keas.Core.Data
 {
-    public class ApplicationDbContext
+    public class ApplicationDbContext : DbContext
     {
+        public ApplicationDbContext(DbContextOptions<ApplicationDbContext> options)
+            : base(options)
+        { }
+        
         public virtual DbSet<User> Users { get; set; }
     }
 }
