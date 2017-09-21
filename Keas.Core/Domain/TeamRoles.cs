a486ed8944e8741d1b0bc1d2ebec98c0041f10cc
James Cubbage <jscubbage@UCDAVIS.EDU>
2017-09-21 16:45:19 -0700

    Add roles

---
+++ Keas.Core/Domain/TeamRoles.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Text;
+
+namespace Keas.Core.Domain
+{
+    public class TeamRoles
+    {
+        [Required]
+        public string Name { get; set; }
+
+        [Required]
+        public Team Team { get; set; }
+
+        [Required]
+        public User User { get; set; }
+
+    }
+}
