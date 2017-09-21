0f3f317626225d9be930b021653199546f2b08f8
James Cubbage <jscubbage@UCDAVIS.EDU>
2017-09-21 16:52:27 -0700

    Assignements

---
+++ Keas.Core/Domain/Assignment.cs
@@ -0,0 +1,23 @@
+﻿
+
+using System;
+using System.ComponentModel.DataAnnotations;
+
+namespace Keas.Core.Domain
+{
+    public class Assignment
+    {
+        [Required]
+        [Key]
+        public int Id { get; set; }
+
+        public TeamMember TeamMember { get; set; }
+
+        public DateTime StartDate { get; set; }
+
+        // TODO: Figue out what appointment info we need. Sync with PPS/Path/IAM????
+
+        public TeamMember Supervisor { get; set; }
+
+    }
+}
