4a4623d8ea7057d96e44c473b02e71b5bd00094d
James Cubbage <jscubbage@UCDAVIS.EDU>
2017-09-21 16:49:11 -0700

    Team members

---
+++ Keas.Core/Domain/TeamMember.cs
@@ -0,0 +1,19 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace Keas.Core.Domain
+{
+    public class TeamMember
+    {
+
+        [Required]
+        [Key]
+        public int Id { get; set; }
+
+        [Required]
+        public Team Team { get; set; }
+
+        [Required]
+        public User User { get; set; }
+
+    }
+}
