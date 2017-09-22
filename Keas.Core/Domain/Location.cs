c8f03dbad713920aacf3473ab1020edf03d5ac45
James Cubbage <jscubbage@UCDAVIS.EDU>
2017-09-21 17:01:12 -0700

    Location

---
+++ Keas.Core/Domain/Location.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+
+namespace Keas.Core.Domain
+{
+    public class Location
+    {
+        [Required]
+        [Key]
+        public int Id { get; set; }
+
+        public string Name { get; set; }
+
+        // TODO: External IDs go here
+
+        public string Type { get; set; }
+
+        [ForeignKey("Id")]
+        public Location ParentLocation { get; set; }
+
+
+    }
+}
