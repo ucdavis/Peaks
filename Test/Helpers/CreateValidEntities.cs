32270192ac07682dd96ea20b2deb09cb74c6287a
Jason Sylvestre <jsylvestre@ucdavis.edu>
2017-10-05 08:22:13 -0700

    Helpers

---
+++ Test/Helpers/CreateValidEntities.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using Keas.Core.Domain;
+
+namespace Test.Helpers
+{
+    public static class CreateValidEntities
+    {
+
+        public static User User(int? counter, bool populateAllFields = false)
+        {
+            var rtValue = new User();
+            rtValue.FirstName = string.Format("FirstName{0}", counter);
+            rtValue.LastName = string.Format("LastName{0}", counter);
+            rtValue.Name = string.Format("{0} {1}", rtValue.FirstName, rtValue.LastName);
+
+            if (populateAllFields)
+            {
+            }
+
+            rtValue.Id = (counter ?? 99).ToString();
+
+            return rtValue;
+
+        }
+    }
+}
