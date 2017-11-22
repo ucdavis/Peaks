e41e0badd146ec7cc9c74efb25b815f07d9cd0cc
Scott Kirkland <srkirkland@gmail.com>
2017-11-22 10:53:29 -0800

    Update db init

--- Keas.Core/Data/ApplicationDbContext.cs
+++ Keas.Core/Data/ApplicationDbContext.cs
@@ -11,10 +11,6 @@ namespace Keas.Core.Data
         
         public virtual DbSet<User> Users { get; set; }
         public virtual DbSet<Team> Teams { get; set; }
-        public virtual DbSet<TeamMember> TeamMembers { get; set; }
-        public virtual DbSet<TeamMembership> TeamRoles { get; set; }
-        public virtual DbSet<Location> Locations { get; set; }
-        public virtual DbSet<Assignment> Assignments { get; set; }
-        public virtual DbSet<Asset> Assets { get; set; }
+        public virtual DbSet<TeamMembership> TeamMemberships { get; set; }
     }
 }
--- Keas.Mvc/Helpers/DbInitializer.cs
+++ Keas.Mvc/Helpers/DbInitializer.cs
@@ -14,28 +14,11 @@ namespace Keas.Mvc.Helpers
             if (context.Users.Any()) return; // already initialzied
 
             var user = new User {Id = "123124", FirstName = "Scott", Name = "Scott Kirkland", Email = "scott@email.com"};
-            var supervisor = new User { Id = "321421", Name = "Adam Getchell", Email = "adam@email.com"};
             var team = new Team {Id = 1, TeamName = "CAES DO"};
-            var teamMember = new TeamMember {Id = 1, Team = team, User = user};
-            var teamMemberSupervisor = new TeamMember {Id = 2, Team = team, User = supervisor};
-            var teamRole = new TeamRole {Name = "Team Admin", TeamMember = teamMemberSupervisor};
-            var locationCampus = new Location {Id = 1, Name = "Davis Campus", Type = "Campus"};
-            var locationBuilding = new Location {Id = 2, Name = "Mrak", Type = "Building", ParentLocation = locationCampus};
-            var locationRoom = new Location {Id = 3, Name = "12", ParentLocation = locationBuilding, Type = "Room"};
-            var assignment = new Assignment {Id = 1, StartDate = DateTime.Now.AddMonths(-2), Assignee = teamMember, Supervisor = teamMemberSupervisor };
-            var asset = new Asset {Id = 1, Type = "Computer", AssignedToTeamMember = teamMember, AssignedByTeamMember = teamMemberSupervisor, Location = locationRoom};
 
             context.Users.Add(user);
-            context.Users.Add(supervisor);
             context.Teams.Add(team);
-            context.TeamMembers.Add(teamMember);
-            context.TeamMembers.Add(teamMemberSupervisor);
-            context.TeamRoles.Add(teamRole);
-            context.Locations.Add(locationCampus);
-            context.Locations.Add(locationBuilding);
-            context.Locations.Add(locationRoom);
-            context.Assignments.Add(assignment);
-            context.Assets.Add(asset);
+            context.TeamMemberships.Add(new TeamMembership { User = user, Team = team, Role = "Admin" });
 
             context.SaveChanges();
             
