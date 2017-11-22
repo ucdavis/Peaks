28005d8cb4a2a56a0e518de2c46f8b43f38a2835
Unknown <jscubbage@ucdavis.edu>
2017-11-22 13:36:20 -0800

    First try super controler and getting Team name

---
+++ Keas.Mvc/Controllers/KeysController.cs
@@ -0,0 +1,51 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Threading.Tasks;
+using Keas.Core.Data;
+using Keas.Core.Domain;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace Keas.Mvc.Controllers
+{
+    public class KeysController : SuperController
+    {
+        private readonly ApplicationDbContext _context;
+
+        public KeysController(ApplicationDbContext context)
+        {
+            this._context = context;
+        }
+
+        public string GetTeam()
+        {
+            return Team;
+        }
+        public async Task<IActionResult> Create(Key key)
+        {
+            // TODO Make sure user has permissions
+            if (ModelState.IsValid)
+            {
+                _context.Keys.Add(key);
+                await _context.SaveChangesAsync();
+            }
+            return Json(key);
+        }
+
+        public async Task<IActionResult> Assign(int keyId, int personId)
+        {
+            // TODO Make sure user has permssion, make sure key exists, makes sure key is in this team
+            if (ModelState.IsValid)
+            {
+                var keyassingment = new KeyAssignment{ KeyId = keyId, PersonId = personId};
+                _context.KeyAssignments.Add(keyassingment);
+                await _context.SaveChangesAsync();
+                return Json(keyassingment);
+            }
+            return BadRequest(ModelState);
+        }
+
+    }
+}
---
+++ Keas.Mvc/Controllers/SuperController.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+
+namespace Keas.Mvc.Controllers
+{
+    public class SuperController : Controller
+    {
+        public string Team => ControllerContext.RouteData.Values["teamName"] as string;
+        
+
+    }
+}
