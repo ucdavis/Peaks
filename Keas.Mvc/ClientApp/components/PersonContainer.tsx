093e6e7a34941ed359713471e49545ed12686147
Unknown <srkirkland@ucdavis.edu>
2017-11-28 11:48:57 -0800

    branch on new person

--- Keas.Mvc/ClientApp/components/PersonContainer.tsx
+++ Keas.Mvc/ClientApp/components/PersonContainer.tsx
@@ -19,6 +19,10 @@ interface IProps {
 
 export default class PersonContainer extends React.Component<IProps, {}> {
   public render() {
+      if (this.props.person.id === 0) {
+          return <div>Create a new person</div>;
+      }
+      
       return <Person person={this.props.person} />;
   }
 }
