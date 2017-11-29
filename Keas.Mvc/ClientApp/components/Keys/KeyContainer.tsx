cfc7044bc4d4008d0678126669051ef2f0897227
Unknown <srkirkland@ucdavis.edu>
2017-11-28 16:42:29 -0800

    playing with fetch

--- Keas.Mvc/ClientApp/components/Keys/KeyContainer.tsx
+++ Keas.Mvc/ClientApp/components/Keys/KeyContainer.tsx
@@ -3,7 +3,7 @@ import "isomorphic-fetch";
 
 import { IPerson } from "../PersonContainer";
 
-import KeyList from './KeyList';
+import KeyList from "./KeyList";
 
 interface IProps {
   person: IPerson;
@@ -23,17 +23,28 @@ export default class KeyContainer extends React.Component<IProps, IState> {
       data: {}
     };
   }
-  componentDidMount() {
+  async componentDidMount() {
     // fetch the keys associated with this user
     // TODO: for now load from SWAPI
-    fetch("https://swapi.co/api/films/2/")
-      .then(r => r.json())
-      .then(data => this.setState({ data, loading: false }))
-      .catch(console.log);
+    // fetch("https://swapi.co/api/films/2/")
+    //   .then(r => r.json())
+    //   .then(data => this.setState({ data, loading: false }))
+    //   .catch(console.log);
+
+    const data = await this.doFetch(fetch("/keys/listassigned/1"));
+    this.setState({ data, loading: false });
   }
   public render() {
     if (this.state.loading) return <h2>Loading...</h2>;
 
     return <KeyList />;
   }
+
+  doFetch = async (p: Promise<Response>) => {
+    const t = await p;
+    if (!t.ok) throw new Error(); 
+
+    const d = await t.json();
+    return d;
+  };
 }
