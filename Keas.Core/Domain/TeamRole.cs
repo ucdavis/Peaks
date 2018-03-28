6ec90c4ef07383aafcec07cf6a8050e3c665c4b0
Unknown <jscubbage@ucdavis.edu>
2018-03-28 14:42:51 -0700

    Team role models

---
+++ Keas.Core/Domain/TeamRole.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Text;
+
+namespace Keas.Core.Domain
+{
+    class TeamRole
+    {
+        [Key]
+        public int Id { get; set; }
+
+        [Required]
+        [StringLength(50)]
+        public string Name { get; set; }
+
+
+    }
+}
