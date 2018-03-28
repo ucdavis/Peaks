176942adc151b186961f19de43cef6be759a4fb4
Unknown <jscubbage@ucdavis.edu>
2018-03-28 14:42:51 -0700

    Team role models

---
+++ Keas.Core/Domain/TeamPermission.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Text;
+
+namespace Keas.Core.Domain
+{
+    class TeamPermission
+    {
+
+        [Required]
+        public Team Team { get; set; }
+        public int TeamId { get; set; }
+
+        [Required]
+        public User User { get; set; }
+        public string UserId { get; set; }
+
+        [Required]
+        public TeamRole TeamRole { get; set; }
+        public int TeamRoleId { get; set; }
+
+    }
+}
