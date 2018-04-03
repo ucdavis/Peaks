eafe659ed5662cb8dd817f613cfac6cfba27c066
Laura Holstege <laholstege@ucdavis.edu>
2018-04-03 11:30:20 -0700

    pull person info from server

--- Keas.Mvc/ClientApp/components/PersonContainer.tsx
+++ Keas.Mvc/ClientApp/components/PersonContainer.tsx
@@ -4,14 +4,31 @@ import * as React from "react";
 import { AppContext, IPerson } from "../Types";
 import Person from "./Person";
 
-export default class PersonContainer extends React.Component<{}, {}> {
+interface IState {
+    person: IPerson;
+}
+
+export default class PersonContainer extends React.Component<{}, IState> {
   public static contextTypes = {
+    fetch: PropTypes.func,
     router: PropTypes.object,
     team: PropTypes.object
   };
   public context: AppContext;
+  constructor(props) {
+      super(props);
+      this.state = {
+          person: null,
+      };
+  }
+
+  public async componentDidMount() {
+      const person = await this.context.fetch(`/people/getPerson?personId=${this.context.router.route.match.params.personId}&teamId=${this.context.team.id}`);
+      this.setState({ person });
+  }
+
   public render() {
-    const personId = this.context.router.route.match.params.id;
+    //const personId = this.context.router.route.match.params.id;
 
     // TODO: go get the person from javascript
     const person: IPerson = {
@@ -25,10 +42,10 @@ export default class PersonContainer extends React.Component<{}, {}> {
     };
 
     // const { person } = this.context;
-    if (!person || person.id === 0) {
+    if (!this.state.person || this.state.person.id === 0) {
       return <div>Create a new person</div>;
     }
 
-    return <Person person={person} />;
+    return <Person person={this.state.person} />;
   }
 }
