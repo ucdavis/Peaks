02c7a538b9f577941dd72cb536a16f337cc92acd
Laura Holstege <laholstege@ucdavis.edu>
2018-04-04 13:22:39 -0700

    show earliest expiring date

--- Keas.Mvc/ClientApp/components/Access/AccessListItem.tsx
+++ Keas.Mvc/ClientApp/components/Access/AccessListItem.tsx
@@ -1,5 +1,5 @@
+import * as moment from "moment";
 import * as React from "react";
-
 import { IAccess } from "../../Types";
 import ListActionsDropdown from "../ListActionsDropdown";
 
@@ -17,12 +17,14 @@ export default class AccessListItem extends React.Component<IProps, {}> {
     public render() {
         const hasAssignment = this.props.accessEntity.assignments.length > 0;
         const canAdd = !this.props.personView || !hasAssignment;
+        const dates = this.props.accessEntity.assignments.map(x => moment(x.expiresAt));
+        const expirationDate = hasAssignment ? moment.min(dates).format("MM/DD/YYYY").toString() : "";
         return (
             <tr>
                 <td>{this.props.accessEntity.name}</td>
                 <td>{hasAssignment ? "Assigned" : "Unassigned"}</td>
                 <td>{this.props.accessEntity.assignments.length}</td>
-                <td></td>
+                <td>{expirationDate}</td>
                 <td>
                     <ListActionsDropdown
                         onRevoke={this._onRevoke}
