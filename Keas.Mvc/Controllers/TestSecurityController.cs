ec4d4a090ccc5423ef780f038780a0b41602fc54
Unknown <jscubbage@ucdavis.edu>
2018-04-16 12:52:08 -0700

    Yet another controller we'll trash later.

---
+++ Keas.Mvc/Controllers/TestSecurityController.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Keas.Core.Data;
+using Keas.Mvc.Services;
+using Microsoft.AspNetCore.Mvc;
+
+namespace Keas.Mvc.Controllers
+{
+    public class TestSecurityController : SuperController
+    {
+        private readonly ISecurityService _securityService;
+        private readonly ApplicationDbContext _context;
+
+        public TestSecurityController(ISecurityService securityService, ApplicationDbContext context)
+        {
+            _securityService = securityService;
+            _context = context;
+        }
+        public IActionResult Index()
+        {
+            ViewBag.Keymaster = _securityService.HasKeyMasterAccess(_context, Team).Result ;
+            return View();
+        }
+    }
+}
