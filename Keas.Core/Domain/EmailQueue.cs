8746322bb4c76e69d7622b0cc30b61a38de2a104
Unknown <jscubbage@ucdavis.edu>
2018-04-25 10:45:00 -0700

    Start queue

---
+++ Keas.Core/Domain/EmailQueue.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using Keas.Core.Helpers;
+
+namespace Keas.Core.Domain
+{
+    public class EmailQueue
+    {
+        public int Id { get; set; }
+
+        public User User { get; set; }
+        public History History { get; set; }
+        public int HistoryId { get; set; }
+        public string Details { get; set; }
+
+        public bool Pending { get; set; }
+        public DateTime? DateTimeSent { get; set; }
+        public string Status { get; set; }
+        public DateTime DateTimeCreated { get; set; }
+
+        public EmailQueue()
+        {
+            Pending = true;
+            DateTimeCreated = DateTime.UtcNow;
+        }
+
+    }
+}
+
