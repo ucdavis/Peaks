a56b49d22a09d715d22ff2b25cfe1785f554b899
Unknown <jscubbage@ucdavis.edu>
2018-04-25 10:41:51 -0700

    History service

---
+++ Keas.Mvc/Services/HistoryService.cs
@@ -0,0 +1,327 @@
+﻿using Keas.Core.Data;
+using Keas.Core.Domain;
+using System.Threading.Tasks;
+
+namespace Keas.Mvc.Services
+{
+    public interface IHistoryService
+    {
+        Task<History> KeyCreated(Key key, User user);
+        Task<History> AccessCreated(Access access, User user);
+        Task<History> EquipmentCreated(Equipment equipment, User user);
+        Task<History> KeyUpdated(Key key, User user);
+        Task<History> AccessUpdated(Access access, User user);
+        Task<History> EquipmentUpdated(Equipment equipment, User user);
+        Task<History> KeyInactivated(Key key, User user);
+        Task<History> AccessInactivated(Access access, User user);
+        Task<History> EquipmentInactivated(Equipment equipment, User user);
+        Task<History> KeyAssigned(Key key, User user);
+        Task<History> AccessAssigned(Access access, User user);
+        Task<History> EquipmentAssigned(Equipment equipment, User user);
+        Task<History> KeyUnassigned(Key key, User user);
+        Task<History> AccessUnassigned(Access access, User user);
+        Task<History> EquipmentUnassigned(Equipment equipment, User user);
+        Task<History> KeyAccepted(Key key, User user);
+        Task<History> AccessAccepted(Access access, User user);
+        Task<History> EquipmentAccepted(Equipment equipment, User user);
+
+    }
+
+    public class HistoryService : IHistoryService
+    {
+        private readonly ApplicationDbContext _context;
+        public HistoryService(ApplicationDbContext context)
+        {
+            _context = context;
+        }
+
+        public async Task<History> KeyCreated(Key key, User user)
+        {
+            var historyEntry = new History
+            {
+                Description = "Key Created",
+                Actor = user,
+                ActorName = user.Name,
+                AssetType = "Key",
+                ActionType = "Created",
+                Key = key
+            };
+            _context.Histories.Add(historyEntry);
+            await _context.SaveChangesAsync();
+            return historyEntry;
+        }
+
+        public async Task<History> AccessCreated(Access access, User user)
+        {
+            var historyEntry = new History
+            {
+                Description = "Access Created",
+                Actor = user,
+                ActorName = user.Name,
+                AssetType = "Access",
+                ActionType = "Created",
+                Access = access
+            };
+            _context.Histories.Add(historyEntry);
+            await _context.SaveChangesAsync();
+            return historyEntry;
+        }
+
+        public async Task<History> EquipmentCreated(Equipment equipment, User user)
+        {
+            var historyEntry = new History
+            {
+                Description = "Equipment Created",
+                Actor = user,
+                ActorName = user.Name,
+                AssetType = "Equipment",
+                ActionType = "Created",
+                Equipment = equipment
+            };
+            _context.Histories.Add(historyEntry);
+            await _context.SaveChangesAsync();
+            return historyEntry;
+        }
+
+        public async Task<History> KeyUpdated(Key key, User user)
+        {
+            var historyEntry = new History
+            {
+                Description = "Key Updated",
+                Actor = user,
+                ActorName = user.Name,
+                AssetType = "Key",
+                ActionType = "Updated",
+                Key = key
+            };
+            _context.Histories.Add(historyEntry);
+            await _context.SaveChangesAsync();
+            return historyEntry;
+        }
+
+        public async Task<History> AccessUpdated(Access access, User user)
+        {
+            var historyEntry = new History
+            {
+                Description = "Access Updated",
+                Actor = user,
+                ActorName = user.Name,
+                AssetType = "Access",
+                ActionType = "Updated",
+                Access = access
+            };
+            _context.Histories.Add(historyEntry);
+            await _context.SaveChangesAsync();
+            return historyEntry;
+        }
+
+        public async Task<History> EquipmentUpdated(Equipment equipment, User user)
+        {
+            var historyEntry = new History
+            {
+                Description = "Equipment Updated",
+                Actor = user,
+                ActorName = user.Name,
+                AssetType = "Equipment",
+                ActionType = "Updated",
+                Equipment = equipment
+            };
+            _context.Histories.Add(historyEntry);
+            await _context.SaveChangesAsync();
+            return historyEntry;
+        }
+        public async Task<History> KeyInactivated(Key key, User user)
+        {
+            var historyEntry = new History
+            {
+                Description = "Key Inactivated",
+                Actor = user,
+                ActorName = user.Name,
+                AssetType = "Key",
+                ActionType = "Inactivated",
+                Key = key
+            };
+            _context.Histories.Add(historyEntry);
+            await _context.SaveChangesAsync();
+            return historyEntry;
+        }
+
+        public async Task<History> AccessInactivated(Access access, User user)
+        {
+            var historyEntry = new History
+            {
+                Description = "Access Inactivated",
+                Actor = user,
+                ActorName = user.Name,
+                AssetType = "Access",
+                ActionType = "Inactivated",
+                Access = access
+            };
+            _context.Histories.Add(historyEntry);
+            await _context.SaveChangesAsync();
+            return historyEntry;
+        }
+
+        public async Task<History> EquipmentInactivated(Equipment equipment, User user)
+        {
+            var historyEntry = new History
+            {
+                Description = "Equipment Inactivated",
+                Actor = user,
+                ActorName = user.Name,
+                AssetType = "Equipment",
+                ActionType = "Inactivated",
+                Equipment = equipment
+            };
+            _context.Histories.Add(historyEntry);
+            await _context.SaveChangesAsync();
+            return historyEntry;
+        }
+
+        public async Task<History> KeyAssigned(Key key, User user)
+        {
+            var historyEntry = new History
+            {
+                Description = "Key Assigned to " + key.Assignment.Person.User.Name,
+                Actor = user,
+                ActorName = user.Name,
+                AssetType = "Key",
+                ActionType = "Assigned",
+                Key = key
+            };
+            _context.Histories.Add(historyEntry);
+            await _context.SaveChangesAsync();
+            return historyEntry;
+        }
+
+        public async Task<History> AccessAssigned(Access access, User user)
+        {
+            var historyEntry = new History
+            {
+                Description = "Access Assigned to ",
+                Actor = user,
+                ActorName = user.Name,
+                AssetType = "Access",
+                ActionType = "Assigned",
+                Access = access
+            };
+            _context.Histories.Add(historyEntry);
+            await _context.SaveChangesAsync();
+            return historyEntry;
+        }
+
+        public async Task<History> EquipmentAssigned(Equipment equipment, User user)
+        {
+            var historyEntry = new History
+            {
+                Description = "Equipment Assigned to " + equipment.Assignment.Person.User.Name,
+                Actor = user,
+                ActorName = user.Name,
+                AssetType = "Equipment",
+                ActionType = "Assigned",
+                Equipment = equipment
+            };
+            _context.Histories.Add(historyEntry);
+            await _context.SaveChangesAsync();
+            return historyEntry;
+        }
+
+        public async Task<History> KeyUnassigned(Key key, User user)
+        {
+            var historyEntry = new History
+            {
+                Description = "Key Unassigned",
+                Actor = user,
+                ActorName = user.Name,
+                AssetType = "Key",
+                ActionType = "Unassigned",
+                Key = key
+            };
+            _context.Histories.Add(historyEntry);
+            await _context.SaveChangesAsync();
+            return historyEntry;
+        }
+
+        public async Task<History> AccessUnassigned(Access access, User user)
+        {
+            var historyEntry = new History
+            {
+                Description = "Access Unassigned",
+                Actor = user,
+                ActorName = user.Name,
+                AssetType = "Access",
+                ActionType = "Unassigned",
+                Access = access
+            };
+            _context.Histories.Add(historyEntry);
+            await _context.SaveChangesAsync();
+            return historyEntry;
+        }
+
+        public async Task<History> EquipmentUnassigned(Equipment equipment, User user)
+        {
+            var historyEntry = new History
+            {
+                Description = "Equipment Unassigned",
+                Actor = user,
+                ActorName = user.Name,
+                AssetType = "Equipment",
+                ActionType = "Unassigned",
+                Equipment = equipment
+            };
+            _context.Histories.Add(historyEntry);
+            await _context.SaveChangesAsync();
+            return historyEntry;
+        }
+
+        public async Task<History> KeyAccepted(Key key, User user)
+        {
+            var historyEntry = new History
+            {
+                Description = "Key Accepted",
+                Actor = user,
+                ActorName = user.Name,
+                AssetType = "Key",
+                ActionType = "Accepted",
+                Key = key
+            };
+            _context.Histories.Add(historyEntry);
+            await _context.SaveChangesAsync();
+            return historyEntry;
+        }
+
+        public async Task<History> AccessAccepted(Access access, User user)
+        {
+            var historyEntry = new History
+            {
+                Description = "Access Accepted",
+                Actor = user,
+                ActorName = user.Name,
+                AssetType = "Access",
+                ActionType = "Accepted",
+                Access = access
+            };
+            _context.Histories.Add(historyEntry);
+            await _context.SaveChangesAsync();
+            return historyEntry;
+        }
+
+        public async Task<History> EquipmentAccepted(Equipment equipment, User user)
+        {
+            var historyEntry = new History
+            {
+                Description = "Equipment Accepted",
+                Actor = user,
+                ActorName = user.Name,
+                AssetType = "Equipment",
+                ActionType = "Accepted",
+                Equipment = equipment
+            };
+            _context.Histories.Add(historyEntry);
+            await _context.SaveChangesAsync();
+            return historyEntry;
+        }
+
+    }
+}
+
