6bfe4a1938b0882f87af64945cc6e42bfdd6768c
Unknown <jscubbage@ucdavis.edu>
2018-05-03 18:26:22 -0700

    Setting up confirm

--- Keas.Core/Domain/AssignmentBase.cs
+++ Keas.Core/Domain/AssignmentBase.cs
@@ -25,5 +25,7 @@ namespace Keas.Core.Domain
         public DateTime ExpiresAt { get; set; }
 
         public bool IsConfirmed { get; set; }
+        public DateTime? ConfirmedAt { get; set; }
+
     }
 }
--- Keas.Mvc/Controllers/ConfirmController.cs
+++ Keas.Mvc/Controllers/ConfirmController.cs
@@ -1,4 +1,5 @@
-﻿using Keas.Core.Data;
+﻿using System;
+using Keas.Core.Data;
 using Keas.Mvc.Models;
 using Keas.Mvc.Services;
 using Microsoft.AspNetCore.Mvc;
@@ -22,8 +23,24 @@ namespace Keas.Mvc.Controllers
         {
             var user = await _securityService.GetUser();
             var viewModel = await ConfirmListModel.Create(Team, _context, user);
+            if (viewModel.Equipment.Count == 0 && viewModel.Keys.Count==0)
+            {
+                Message = "You have no pending items to accept";
+                // TODO Find good place to redirect to. Ignores redirects to non-async actionresults.
+                RedirectToAction("Index", "Home");
+            }
 
             return View(viewModel);
+        }
+
+        public async Task<IActionResult> AcceptKey(int keyid)
+        {
+            var keyAssignment = _context.Keys.Where(k => k.Id == keyid).Select(ka => ka.Assignment).First();
+            keyAssignment.IsConfirmed = true;
+            keyAssignment.ConfirmedAt = DateTime.UtcNow;
+            _context.Update(keyAssignment);
+
+            return RedirectToAction(nameof(Index));
         }
     }
 }
