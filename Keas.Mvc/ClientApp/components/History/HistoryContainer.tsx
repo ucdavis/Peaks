206b5a267cc8c0ddc396769810c2f164111464b7
Laura Holstege <laholstege@ucdavis.edu>
2018-05-10 11:54:03 -0700

    reload button

--- Keas.Mvc/ClientApp/components/History/HistoryContainer.tsx
+++ Keas.Mvc/ClientApp/components/History/HistoryContainer.tsx
@@ -1,13 +1,16 @@
 import PropTypes from "prop-types";
 import * as React from "react";
+import { Button } from "reactstrap";
 
 import { AppContext, IHistory, IPerson } from "../../Types";
 
 import HistoryList from "./HistoryList";
 
 interface IState {
-  loading: boolean;
   histories: IHistory[];
+  loading: boolean;
+  reloaded: boolean;
+  reloading: boolean;
 }
 
 interface IProps {
@@ -28,17 +31,19 @@ export default class HistoryContainer extends React.Component<IProps, IState> {
 
     this.state = {
       histories: [],
-      loading: true
+      loading: true,
+      reloaded: false,
+      reloading: false,
     };
   }
   public async componentDidMount() {
-    // are we getting the person's key or the team's?
-    const historyFetchUrl = this.props.person
+      this.setState({loading: true});
+      const historyFetchUrl = this.props.person
       ? `/api/${this.context.team.name}/people/getHistory/${this.props.person.id}`
       : `/api/${this.context.team.name}/${this.props.type}/getHistory/${this.props.id}`;
 
     const histories = await this.context.fetch(historyFetchUrl);
-    this.setState({ histories, loading: false });
+    this.setState({ histories, loading: false });  
   }
   public render() {
     if (this.state.loading) {
@@ -54,9 +59,25 @@ export default class HistoryContainer extends React.Component<IProps, IState> {
           {this.state.histories.length < 1 &&
             <p>No histories were found</p>
           }
+          {!!this.props.person &&
+            <Button onClick={this._loadHistories} disabled={this.state.reloading}>
+              Reload Histories {" "}
+              {this.state.reloaded ? <i className="fa fa-check" /> : null}
+              {this.state.reloading ? <i className="fa fa-spin fa-circle-o-notch" /> : null}
+            </Button> }
         </div>
       </div>
     );
+  }
+
+  private _loadHistories = async () => {
+        this.setState({reloading: true, reloaded: false});
+        const historyFetchUrl = this.props.person
+        ? `/api/${this.context.team.name}/people/getHistory/${this.props.person.id}`
+        : `/api/${this.context.team.name}/${this.props.type}/getHistory/${this.props.id}`;
+  
+      const histories = await this.context.fetch(historyFetchUrl);
+      this.setState({ histories, reloading: false, reloaded: true });
   }
 
 }
