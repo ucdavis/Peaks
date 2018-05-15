03a615a73d5f59a5a580f9b04851f209860bec43
Unknown <jscubbage@ucdavis.edu>
2018-05-15 15:38:04 -0700

    Workstations are now a thing.

---
+++ Keas.Core/Domain/Workstation.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections.Generic;
+
+namespace Keas.Core.Domain
+{
+    public class Workstation : AssetBase
+    {
+        public Workstation()
+        {
+            Attributes = new List<WorkstationAttribute>();
+        }
+
+        // Desk Table ???
+        public string Type { get; set; }
+        
+        public Space Space { get; set; }
+        public int SpaceId { get; set; }
+
+        public WorkstationAssignment Assignment { get; set; }
+        public int? WorkstationAssignmentId { get; set; }
+
+        public List<WorkstationAttribute> Attributes { get; set; }
+
+        public void AddAttribute(string key, string value)
+        {
+            Attributes.Add(new WorkstationAttribute { Workstation = this, Key = key, Value = value });
+        }
+    }
+}
