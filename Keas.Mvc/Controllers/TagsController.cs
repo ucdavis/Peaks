1a71630d639021bc99699b2d96f623d057ff0c66
Unknown <jscubbage@ucdavis.edu>
2018-06-05 14:25:26 -0700

    Tags controller

---
+++ Keas.Mvc/Controllers/TagsController.cs
@@ -0,0 +1,98 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Keas.Core.Data;
+using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace Keas.Mvc.Controllers
+{
+    [Authorize(Policy = "DepartmentAdminAccess")]
+    public class TagsController : SuperController
+    {
+        private readonly ApplicationDbContext _context;
+
+        public TagsController(ApplicationDbContext context)
+        {
+            _context = context;
+        }
+
+        // GET: Tags
+        public ActionResult Index()
+        {
+            var model = _context.TeamTags.Where(t => t.Team.Name == Team).OrderBy(t=> t.Tag).ToList();
+            return View(model);
+        }
+
+        // GET: Tags/Create
+        public ActionResult Create()
+        {
+            return View();
+        }
+
+        // POST: Tags/Create
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Create(IFormCollection collection)
+        {
+            try
+            {
+                // TODO: Add insert logic here
+
+                return RedirectToAction(nameof(Index));
+            }
+            catch
+            {
+                return View();
+            }
+        }
+
+        // GET: Tags/Edit/5
+        public ActionResult Edit(int id)
+        {
+            return View();
+        }
+
+        // POST: Tags/Edit/5
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Edit(int id, IFormCollection collection)
+        {
+            try
+            {
+                // TODO: Add update logic here
+
+                return RedirectToAction(nameof(Index));
+            }
+            catch
+            {
+                return View();
+            }
+        }
+
+        // GET: Tags/Delete/5
+        public ActionResult Delete(int id)
+        {
+            return View();
+        }
+
+        // POST: Tags/Delete/5
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Delete(int id, IFormCollection collection)
+        {
+            try
+            {
+                // TODO: Add delete logic here
+
+                return RedirectToAction(nameof(Index));
+            }
+            catch
+            {
+                return View();
+            }
+        }
+    }
+}
