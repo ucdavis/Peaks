81b5fba4afec01c5ed7d4a0193d6d8f6a71db450
Jason Sylvestre <jsylvestre@ucdavis.edu>
2018-07-23 07:56:50 -0700

    Email Job
    Create basic structure

---
+++ Keas.Jobs.SendMail/Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.IO;
+using System.Linq;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+
+namespace Keas.Jobs.SendMail
+{
+    public class Program
+    {
+        public static IConfigurationRoot Configuration { get; set; }
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
