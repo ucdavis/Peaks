a8f87e14b8736e713ffcd99592c1f0e436b1cbc1
Laura Holstege <laholstege@ucdavis.edu>
2018-08-16 16:38:42 -0700

    render name properly

--- Keas.Mvc/ClientApp/components/Workstations/AssignWorkstation.tsx
+++ Keas.Mvc/ClientApp/components/Workstations/AssignWorkstation.tsx
@@ -96,6 +96,7 @@ export default class AssignWorkstation extends React.Component<IProps, IState> {
                       tags={this.props.tags}
                       selectedWorkstation={this.state.workstation}
                       changeProperty={this._changeProperty}
+                      creating={true}
                       disableEditing={false}
                     />
                   }
