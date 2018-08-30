7a0f69510e8f5032c743033d0d3fd5db4961b642
Jason Sylvestre <jsylvestre@ucdavis.edu>
2018-08-30 10:32:32 -0700

    Last of the basic db tests

---
+++ Test/TestsDatabase/WorkstationAttributeTests.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using Keas.Core.Domain;
+using TestHelpers.Helpers;
+using Xunit;
+
+namespace Test.TestsDatabase
+{
+    [Trait("Category","DatabaseTests")]
+    public class WorkstationAttributeTests
+    {
+        #region Reflection of Database
+
+        [Fact]
+        public void TestDatabaseFieldAttributes()
+        {
+            #region Arrange
+            var expectedFields = new List<NameAndType>();
+            expectedFields.Add(new NameAndType("Id", "System.Int32", new List<string>()));
+            expectedFields.Add(new NameAndType("Key", "System.String", new List<string>()));
+            expectedFields.Add(new NameAndType("Value", "System.String", new List<string>()));
+            expectedFields.Add(new NameAndType("Workstation", "Keas.Core.Domain.Workstation", new List<string>()));
+            expectedFields.Add(new NameAndType("WorkstationId", "System.Int32", new List<string>()));
+            #endregion Arrange
+
+            AttributeAndFieldValidation.ValidateFieldsAndAttributes(expectedFields, typeof(WorkstationAttribute));
+        }
+
+        #endregion Reflection of Database
+    }
+}
