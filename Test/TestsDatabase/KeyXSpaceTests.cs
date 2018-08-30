4cde52fbc22674442cc06ce0492c80e637bd980a
Jason Sylvestre <jsylvestre@ucdavis.edu>
2018-08-30 08:30:06 -0700

    more db tests

---
+++ Test/TestsDatabase/KeyXSpaceTests.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using Keas.Core.Domain;
+using TestHelpers.Helpers;
+using Xunit;
+
+namespace Test.TestsDatabase
+{
+    [Trait("Category","DatabaseTests")]
+    public class KeyXSpaceTests
+    {
+        #region Reflection of Database
+
+        [Fact]
+        public void TestDatabaseFieldAttributes()
+        {
+            #region Arrange
+            var expectedFields = new List<NameAndType>();
+            expectedFields.Add(new NameAndType("Id", "System.Int32", new List<string>
+            {
+                "[System.ComponentModel.DataAnnotations.KeyAttribute()]",
+            }));
+            expectedFields.Add(new NameAndType("Key", "Keas.Core.Domain.Key", new List<string>
+            {
+                "[System.ComponentModel.DataAnnotations.RequiredAttribute()]",
+            }));
+            expectedFields.Add(new NameAndType("KeyId", "System.Int32", new List<string>()));
+            expectedFields.Add(new NameAndType("Space", "Keas.Core.Domain.Space", new List<string>
+            {
+                "[System.ComponentModel.DataAnnotations.RequiredAttribute()]",
+            }));
+
+            expectedFields.Add(new NameAndType("SpaceId", "System.Nullable`1[System.Int32]", new List<string>()));
+
+            #endregion Arrange
+
+            AttributeAndFieldValidation.ValidateFieldsAndAttributes(expectedFields, typeof(KeyXSpace));
+        }
+
+        #endregion Reflection of Database
+    }
+}
