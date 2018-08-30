268def9d026e713c309ae4a49ddc6e025aea31fa
Jason Sylvestre <jsylvestre@ucdavis.edu>
2018-08-30 09:51:25 -0700

    more db tests

---
+++ Test/TestsDatabase/SerialTests.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using Keas.Core.Domain;
+using TestHelpers.Helpers;
+using Xunit;
+
+namespace Test.TestsDatabase
+{
+    [Trait("Category","DatabaseTests")]
+    public class SerialTests
+    {
+        #region Reflection of Database
+
+        [Fact]
+        public void TestDatabaseFieldAttributes()
+        {
+            #region Arrange
+            var expectedFields = new List<NameAndType>();
+            expectedFields.Add(new NameAndType("Active", "System.Boolean", new List<string>()));
+            expectedFields.Add(new NameAndType("Assignment", "Keas.Core.Domain.KeyAssignment", new List<string>()));
+            expectedFields.Add(new NameAndType("Id", "System.Int32", new List<string>
+            {
+                "[System.ComponentModel.DataAnnotations.KeyAttribute()]",
+            }));
+            expectedFields.Add(new NameAndType("Key", "Keas.Core.Domain.Key", new List<string>()));
+            expectedFields.Add(new NameAndType("KeyAssignmentId", "System.Nullable`1[System.Int32]", new List<string>()));
+            expectedFields.Add(new NameAndType("KeyId", "System.Int32", new List<string>()));
+            expectedFields.Add(new NameAndType("Number", "System.String", new List<string>()));           
+            #endregion Arrange
+
+            AttributeAndFieldValidation.ValidateFieldsAndAttributes(expectedFields, typeof(Serial));
+        }
+
+        #endregion Reflection of Database
+    }
+}
