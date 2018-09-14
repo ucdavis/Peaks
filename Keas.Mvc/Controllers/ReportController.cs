6ea09f2be428348bde51342a7ae243f7f952ee2e
Unknown <jscubbage@ucdavis.edu>
2018-09-14 12:03:40 -0700

    Add security role

--- Keas.Mvc/Controllers/ReportController.cs
+++ Keas.Mvc/Controllers/ReportController.cs
@@ -4,10 +4,12 @@ using System.Linq;
 using System.Threading.Tasks;
 using Keas.Core.Data;
 using Keas.Mvc.Models;
+using Microsoft.AspNetCore.Authorization;
 using Microsoft.AspNetCore.Mvc;
 
 namespace Keas.Mvc.Controllers
 {
+    [Authorize(Policy = "AnyRole")]
     public class ReportController : SuperController
     {
         private readonly ApplicationDbContext _context;
