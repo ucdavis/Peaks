95d7aaae3ba31f70360d99af553dea8c81e6998c
James Cubbage <jscubbage@ucdavis.edu>
2018-10-02 12:30:51 -0700

    Split reports, moved to partials

--- Keas.Mvc/Controllers/ReportController.cs
+++ Keas.Mvc/Controllers/ReportController.cs
@@ -28,5 +28,53 @@ namespace Keas.Mvc.Controllers
             var model = await ExpiringItemsViewModel.Create(_context, expiresBefore.Value, Team, showInactive, showType);
             return View(model);
         }
+
+        [Authorize(Policy = "KeyMasterAccess")]
+        public async Task<ActionResult> ExpiringKeys (bool showInactive = false, DateTime? expiresBefore = null)
+        {
+            string showType = "Key";
+            if (expiresBefore == null)
+            {
+                expiresBefore = DateTime.Now.AddDays(30);
+            }
+            var model = await ExpiringItemsViewModel.Create(_context, expiresBefore.Value, Team, showInactive, showType);
+            return View(model);
+        }
+
+        [Authorize(Policy = "EquipMasterAccess")]
+        public async Task<ActionResult> ExpiringEquipment (bool showInactive = false, DateTime? expiresBefore = null)
+        {
+            string showType = "Equipment";
+            if (expiresBefore == null)
+            {
+                expiresBefore = DateTime.Now.AddDays(30);
+            }
+            var model = await ExpiringItemsViewModel.Create(_context, expiresBefore.Value, Team, showInactive, showType);
+            return View(model);
+        }
+
+        [Authorize(Policy = "AccessMasterAccess")]
+        public async Task<ActionResult> ExpiringAccess (bool showInactive = false, DateTime? expiresBefore = null)
+        {
+            string showType = "Access";
+            if (expiresBefore == null)
+            {
+                expiresBefore = DateTime.Now.AddDays(30);
+            }
+            var model = await ExpiringItemsViewModel.Create(_context, expiresBefore.Value, Team, showInactive, showType);
+            return View(model);
+        }
+
+        [Authorize(Policy = "SpaceMasterAccess")]
+        public async Task<ActionResult> ExpiringWorkstations (bool showInactive = false, DateTime? expiresBefore = null)
+        {
+            string showType = "Workstation";
+            if (expiresBefore == null)
+            {
+                expiresBefore = DateTime.Now.AddDays(30);
+            }
+            var model = await ExpiringItemsViewModel.Create(_context, expiresBefore.Value, Team, showInactive, showType);
+            return View(model);
+        }
     }
 }
