3e293554101347be4b07d30aacef174c109d95f6
Jason Sylvestre <jsylvestre@ucdavis.edu>
2018-10-02 10:16:04 -0700

    Default not to reset the Db
    Created a new System controller

---
+++ Keas.Mvc/Controllers/SystemController.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Keas.Core.Data;
+using Keas.Mvc.Helpers;
+using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Mvc;
+
+namespace Keas.Mvc.Controllers
+{
+    public class SystemController : SuperController
+    {
+        private readonly ApplicationDbContext _context;
+
+        public SystemController(ApplicationDbContext context)
+        {
+            _context = context;
+        }
+        
+        public IActionResult ResetDb()
+        {
+#if DEBUG
+            DbInitializer.Initialize(_context);
+            return Content("Success");
+#else
+            return Content("Not in Debug mode");
+#endif
+        }
+    }
+}
