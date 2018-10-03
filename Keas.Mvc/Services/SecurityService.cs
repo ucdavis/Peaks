16b054c4e601efeb4e127223157e1b94a677ceee
James Cubbage <jscubbage@ucdavis.edu>
2018-10-03 13:41:54 -0700

    One more slug.

--- Keas.Mvc/Services/SecurityService.cs
+++ Keas.Mvc/Services/SecurityService.cs
@@ -60,7 +60,7 @@ namespace Keas.Mvc.Services
 
         public async Task<bool> IsInRoles(List<Role> roles, string teamSlug, User user)
         {
-             var team = await _dbContext.Teams.SingleAsync(t => t.Name == teamSlug);
+             var team = await _dbContext.Teams.SingleAsync(t => t.Slug == teamSlug);
             var roleIds = roles.Select(a => a.Id).ToArray();
             if (await _dbContext.TeamPermissions.AnyAsync(a => a.Team == team && a.User == user && roleIds.Contains(a.RoleId)))
             {
