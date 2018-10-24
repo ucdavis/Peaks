64ec4be21d840f28074188ce8db05e736c85b99d
James Cubbage <jscubbage@ucdavis.edu>
2018-10-24 15:11:29 -0700

    And make it work for sysAdmin pages.

--- Keas.Mvc/Handlers/VerifyRoleAccessHandler.cs
+++ Keas.Mvc/Handlers/VerifyRoleAccessHandler.cs
@@ -42,8 +42,12 @@ namespace Keas.Mvc.Handlers
             }  
             if (string.IsNullOrWhiteSpace(team)) 
             {
-                var tempData = _tempDataDictionaryFactory.GetTempData(_httpContext.HttpContext);                
-                team = tempData["TeamName"].ToString();
+                var tempData = _tempDataDictionaryFactory.GetTempData(_httpContext.HttpContext);   
+                var tempName =  tempData["TeamName"];         
+                if (tempName != null)
+                {
+                    team = tempData["TeamName"].ToString();
+                }                
             }         
 
             var user = _dbContext.Users.SingleOrDefault(u => u.Id == context.User.Identity.Name);
