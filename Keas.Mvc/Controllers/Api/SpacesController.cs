012a7c57ad58c1b451033437a58ad21764ef81db
Unknown <srkirkland@ucdavis.edu>
2018-10-25 17:13:52 -0700

    add dapper and map

--- Keas.Mvc/Controllers/Api/SpacesController.cs
+++ Keas.Mvc/Controllers/Api/SpacesController.cs
@@ -2,6 +2,7 @@
 using System.Linq;
 using System.Net;
 using System.Threading.Tasks;
+using Dapper;
 using Keas.Core.Data;
 using Keas.Core.Domain;
 using Microsoft.AspNetCore.Http;
@@ -35,27 +36,52 @@ namespace Keas.Mvc.Controllers.Api
         {
             //TODO clean up workstations query
             var orgIds = await _context.FISOrgs.Where(f => f.Team.Slug == Team).Select(x => x.OrgCode).Distinct().ToListAsync();
-            var spaces =
-                from space in _context.Spaces.Where(x => orgIds.Contains(x.OrgId))
-                select new
-                {
-                    space = space,
-                    id = space.Id,
-                    equipmentCount =
-                        (from eq in _context.Equipment where eq.SpaceId == space.Id && eq.Active select eq).Count(),
-                    keyCount =
-                        (from k in _context.KeyXSpaces where k.SpaceId == space.Id && k.Key.Active select k.Key).Count(),
-                    workstationsTotal =
-                        (from w in _context.Workstations where w.SpaceId == space.Id && w.Active select w).Count(),
-                    workstationsInUse =
-                        (from w in _context.Workstations where w.SpaceId == space.Id && w.Active && w.Assignment != null select w).Count(),
-                    tags =
-                        string.Join(",",
-                        (from w in _context.Workstations
-                         where w.SpaceId == space.Id && w.Active && !string.IsNullOrWhiteSpace(w.Tags)
-                         select w.Tags).ToArray()),
-                };
-            return Json(await spaces.ToListAsync());
+            
+            var sql = @"select Space.*,
+       Space.Id              as Id,
+       EquipmentCount,
+       COALESCE(KeyCount, 0) as KeyCount,
+       WorkstationsTotalCount,
+       WorkstationsInUseCount,
+       COALESCE(WorkstationTags, '') as tags
+from (select Space.Id, count(Equipment.Id) as EquipmentCount
+      from Spaces Space
+             left join Equipment on Space.Id = Equipment.SpaceId and Equipment.Active = 1
+      group by Space.Id) t1
+       left outer join (select Space.Id, count(KeyXSpaces.Id) as KeyCount
+                        from Spaces Space
+                               left join KeyXSpaces on Space.Id = KeyXSpaces.SpaceId
+                               inner join Keys K on KeyXSpaces.KeyId = K.Id and K.Active = 1
+                        group by Space.Id) t3 on t1.Id = t3.Id
+       left outer join (select Space.Id, count(W.Id) as WorkstationsTotalCount, STRING_AGG(Tags, ',') as WorkstationTags
+                        from Spaces Space
+                               left join Workstations W on Space.Id = W.SpaceId and W.Active = 1
+                        group by Space.Id) t2 on t1.Id = t2.Id
+       left outer join (select Space.Id, count(W.Id) as WorkstationsInUseCount
+                        from Spaces Space
+                               left join Workstations W
+                                 on Space.Id = W.SpaceId and W.Active = 1 and W.WorkstationAssignmentId is not null
+                        group by Space.Id) t4 on t1.Id = t4.Id
+       inner join Spaces Space on Space.Id = t1.Id;";
+
+            var result = await _context.Database.GetDbConnection().QueryAsync(sql);
+
+            var spaces = result.Select(r => new {
+                space = new {
+                    id = r.Id,
+                    bldgName = r.BldgName,
+                    roomNumber = r.RoomNumber,
+                    roomName = r.RoomName
+                },
+                id = r.Id,
+                equipmentCount = r.EquipmentCount,
+                keyCount = r.KeyCount,
+                workstationsTotal = r.WorkstationsTotalCount,
+                workstationsInUse = r.WorkstationsInUseCount,
+                tags = r.Tags
+            });
+
+            return Json(spaces);
         }
 
         public async Task<IActionResult> GetTagsInSpace(int spaceId)
