2943b49e5738216f861d9a52d54d3bb401f5df43
Unknown <srkirkland@ucdavis.edu>
2018-10-29 10:22:05 -0700

    move to separate query resources

--- Keas.Mvc/Controllers/Api/SpacesController.cs
+++ Keas.Mvc/Controllers/Api/SpacesController.cs
@@ -34,37 +34,10 @@ namespace Keas.Mvc.Controllers.Api
 
         public async Task<IActionResult> List()
         {
-            //TODO clean up workstations query
+            //TODO clean up workstations query or integrate with list query
             var orgIds = await _context.FISOrgs.Where(f => f.Team.Slug == Team).Select(x => x.OrgCode).Distinct().ToArrayAsync();
-            
-            // TODO: filter by ORGIDs
-            var sql = @"select Space.*,
-       Space.Id              as Id,
-       EquipmentCount,
-       COALESCE(KeyCount, 0) as KeyCount,
-       WorkstationsTotalCount,
-       WorkstationsInUseCount,
-       COALESCE(WorkstationTags, '') as tags
-from (select Space.Id, count(Equipment.Id) as EquipmentCount
-      from Spaces Space
-             left join Equipment on Space.Id = Equipment.SpaceId and Equipment.Active = 1
-      group by Space.Id) t1
-       left outer join (select Space.Id, count(KeyXSpaces.Id) as KeyCount
-                        from Spaces Space
-                               left join KeyXSpaces on Space.Id = KeyXSpaces.SpaceId
-                               inner join Keys K on KeyXSpaces.KeyId = K.Id and K.Active = 1
-                        group by Space.Id) t3 on t1.Id = t3.Id
-       left outer join (select Space.Id, count(W.Id) as WorkstationsTotalCount, STRING_AGG(Tags, ',') as WorkstationTags
-                        from Spaces Space
-                               left join Workstations W on Space.Id = W.SpaceId and W.Active = 1
-                        group by Space.Id) t2 on t1.Id = t2.Id
-       left outer join (select Space.Id, count(W.Id) as WorkstationsInUseCount
-                        from Spaces Space
-                               left join Workstations W
-                                 on Space.Id = W.SpaceId and W.Active = 1 and W.WorkstationAssignmentId is not null
-                        group by Space.Id) t4 on t1.Id = t4.Id
-       inner join Spaces Space on Space.Id = t1.Id
-       where Space.OrgId in (@orgIds);";
+
+            var sql = SpaceQueries.List;            
 
             var result = await _context.Database.GetDbConnection().QueryAsync(sql, new { orgIds });
 
---
+++ Keas.Mvc/Resources/SpaceQueries.cs
@@ -0,0 +1,29 @@
+public static class SpaceQueries {
+    public static string List = @"select Space.*,
+       Space.Id              as Id,
+       EquipmentCount,
+       COALESCE(KeyCount, 0) as KeyCount,
+       WorkstationsTotalCount,
+       WorkstationsInUseCount,
+       COALESCE(WorkstationTags, '') as tags
+from (select Space.Id, count(Equipment.Id) as EquipmentCount
+      from Spaces Space
+             left join Equipment on Space.Id = Equipment.SpaceId and Equipment.Active = 1
+      group by Space.Id) t1
+       left outer join (select Space.Id, count(KeyXSpaces.Id) as KeyCount
+                        from Spaces Space
+                               left join KeyXSpaces on Space.Id = KeyXSpaces.SpaceId
+                               inner join Keys K on KeyXSpaces.KeyId = K.Id and K.Active = 1
+                        group by Space.Id) t3 on t1.Id = t3.Id
+       left outer join (select Space.Id, count(W.Id) as WorkstationsTotalCount, STRING_AGG(Tags, ',') as WorkstationTags
+                        from Spaces Space
+                               left join Workstations W on Space.Id = W.SpaceId and W.Active = 1
+                        group by Space.Id) t2 on t1.Id = t2.Id
+       left outer join (select Space.Id, count(W.Id) as WorkstationsInUseCount
+                        from Spaces Space
+                               left join Workstations W
+                                 on Space.Id = W.SpaceId and W.Active = 1 and W.WorkstationAssignmentId is not null
+                        group by Space.Id) t4 on t1.Id = t4.Id
+       inner join Spaces Space on Space.Id = t1.Id
+       where Space.OrgId in (@orgIds);";
+}
