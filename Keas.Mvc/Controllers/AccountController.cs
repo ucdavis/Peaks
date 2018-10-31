84d2d657d88a4550d9acc8f162d023cd2502da39
John Knoll <jpknoll@gmail.com>
2018-10-31 15:54:07 -0700

    tweak authentication scheme

--- Keas.Mvc/Controllers/AccountController.cs
+++ Keas.Mvc/Controllers/AccountController.cs
@@ -1,21 +1,25 @@
 ﻿using System;
-using System.Collections.Generic;
-using System.Linq;
 using System.Threading.Tasks;
+using AspNetCore.Security.CAS;
+using Microsoft.AspNetCore.Authentication;
+using Microsoft.AspNetCore.Authorization;
 using Microsoft.AspNetCore.Mvc;
 
 namespace Keas.Mvc.Controllers
 {
     public class AccountController : Controller
     {
-        public IActionResult Index()
+        public IActionResult AccessDenied()
         {
             return View();
         }
 
-        public IActionResult AccessDenied()
+        [AllowAnonymous]
+        [Route("login")]
+        public async Task Login(string returnUrl)
         {
-            return View();
+            var props = new AuthenticationProperties { RedirectUri = returnUrl };
+            await HttpContext.ChallengeAsync(CasDefaults.AuthenticationScheme, props);
         }
     }
 }
