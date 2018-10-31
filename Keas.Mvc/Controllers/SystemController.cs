b980c6d7e20961707cc05e9e0507762dc4e45502
John Knoll <jpknoll@gmail.com>
2018-10-31 15:59:02 -0700

    emulate function

--- Keas.Mvc/Controllers/SystemController.cs
+++ Keas.Mvc/Controllers/SystemController.cs
@@ -1,31 +1,71 @@
 ﻿using System;
-using System.Collections.Generic;
-using System.Linq;
+using System.Security.Claims;
 using System.Threading.Tasks;
 using Keas.Core.Data;
 using Keas.Mvc.Helpers;
+using Keas.Mvc.Models;
+using Microsoft.AspNetCore.Authentication;
+using Microsoft.AspNetCore.Authentication.Cookies;
 using Microsoft.AspNetCore.Authorization;
 using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
 
 namespace Keas.Mvc.Controllers
 {
+    [Authorize(Policy = "SystemAdminAccess")]
     public class SystemController : SuperController
     {
         private readonly ApplicationDbContext _context;
 
-        public SystemController(ApplicationDbContext context)
+        public SystemController(ApplicationDbContext context, IAuthenticationService authenticationService)
         {
             _context = context;
         }
         
+#if DEBUG
         public IActionResult ResetDb()
         {
-#if DEBUG
             DbInitializer.Initialize(_context);
             return Content("Success");
-#else
-            return Content("Not in Debug mode");
+        }
 #endif
+
+        [HttpGet]
+        [Authorize()]
+        public IActionResult Emulate()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        [Authorize()]
+        public async Task<IActionResult> Emulate(EmulateUserViewModel model)
+        {
+            //Log.Information("Emulation attempted for {username}", username);
+
+            var user = await _context.Users.SingleOrDefaultAsync(u => u.Email == model.UserEmail || u.Id == model.UserEmail);
+            if (user == null)
+            {
+                return View(model);
+            }
+
+            var identity = new ClaimsIdentity(new[]
+            {
+                new Claim(ClaimTypes.NameIdentifier, user.Id),
+                new Claim(ClaimTypes.Name, user.Id),
+                new Claim(ClaimTypes.GivenName, user.FirstName),
+                new Claim(ClaimTypes.Surname, user.LastName),
+                new Claim("name", user.Name),
+                new Claim(ClaimTypes.Email, user.Email)
+            }, CookieAuthenticationDefaults.AuthenticationScheme);
+
+            // kill old login
+            await HttpContext.SignOutAsync(CookieAuthenticationDefaults.AuthenticationScheme);
+
+            // create new login
+            await HttpContext.SignInAsync(CookieAuthenticationDefaults.AuthenticationScheme, new ClaimsPrincipal(identity));
+
+            return RedirectToAction("Index", "Home");
         }
     }
 }
---
+++ Keas.Mvc/Models/EmulateUserViewModel.cs
@@ -0,0 +1,11 @@
+﻿using System;
+using System.ComponentModel.DataAnnotations;
+
+namespace Keas.Mvc.Models
+{
+    public class EmulateUserViewModel
+    {
+        [Display(Name = "Search by Email")]
+        public string UserEmail { get; set; }
+    }
+}
