8a87635f9cdc9ef95ba679ce970e4cb7eb1b6dce
John Knoll <jpknoll@gmail.com>
2018-11-08 13:55:52 -0800

    fix some things

--- Keas.Core/Models/ExpiringItemsEmailModel.cs
+++ Keas.Core/Models/ExpiringItemsEmailModel.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+using System.Collections.Generic;
 using System.Linq;
 using Keas.Core.Domain;
 
@@ -7,7 +8,7 @@ namespace Keas.Core.Models
     public class ExpiringItemsEmailModel
     {
         public IList<AccessAssignment> AccessAssignments { get; set; }
-        public IList<Serial> Keys { get; set; }
+        public IList<KeySerial> KeySerials { get; set; }
         public IList<Equipment> Equipment { get; set; }
         public IList<Workstation> Workstations { get; set; }
 
@@ -15,7 +16,7 @@ namespace Keas.Core.Models
         public Person Person { get; set; }
 
 
-        public static ExpiringItemsEmailModel Create(IList<AccessAssignment> access, IList<Serial> keys, IList<Equipment> equipment, IList<Workstation> workstations, Person person)
+        public static ExpiringItemsEmailModel Create(IList<AccessAssignment> access, IList<KeySerial> keySerials, IList<Equipment> equipment, IList<Workstation> workstations, Person person)
         {
             var viewModel = new ExpiringItemsEmailModel
             {
@@ -31,7 +32,7 @@ namespace Keas.Core.Models
         public IList<int> GetPersonIdList()
         {
             return AccessAssignments.Select(a => a.PersonId).Distinct()
-                .Union(Keys.Select(a => a.Assignment.PersonId).Distinct())
+                .Union(KeySerials.Select(a => a.Assignment.PersonId).Distinct())
                 .Union(Equipment.Select(a => a.Assignment.PersonId).Distinct())
                 .Union(Workstations.Select(a => a.Assignment.PersonId)).Distinct().ToList();
         }
