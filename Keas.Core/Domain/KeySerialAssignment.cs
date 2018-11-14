06ce6fc07d0114e91e63f8051b8f9800c1108df9
John Knoll <jpknoll@gmail.com>
2018-11-14 09:07:39 -0800

    setup one to one/zero

--- Keas.Core/Domain/KeySerialAssignment.cs
+++ Keas.Core/Domain/KeySerialAssignment.cs
@@ -1,6 +1,11 @@
+using Microsoft.EntityFrameworkCore;
+
 namespace Keas.Core.Domain
 {
-    public class KeySerialAssignment : AssignmentBase {
-        
+    public class KeySerialAssignment : AssignmentBase
+    {
+        public int KeySerialId { get; set; }
+
+        public KeySerial KeySerial { get; set; }
     }
 }
