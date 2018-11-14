be9540202ea59c31c35a1dcf7da6efad8f8744af
Jason Sylvestre <jsylvestre@ucdavis.edu>
2018-11-14 09:40:01 -0800

    update tests

--- Test/TestsDatabase/KeyAssignmentTests.cs
+++ Test/TestsDatabase/KeyAssignmentTests.cs
@@ -44,13 +44,14 @@ namespace Test.TestsDatabase
             }));
             expectedFields.Add(new NameAndType("Id", "System.Int32", new List<string>()));
             expectedFields.Add(new NameAndType("IsConfirmed", "System.Boolean", new List<string>()));
+            expectedFields.Add(new NameAndType("KeySerial", "Keas.Core.Domain.KeySerial", new List<string>()));
+            expectedFields.Add(new NameAndType("KeySerialId", "System.Int32", new List<string>()));
             expectedFields.Add(new NameAndType("NextNotificationDate", "System.Nullable`1[System.DateTime]", new List<string>()));
             expectedFields.Add(new NameAndType("Person", "Keas.Core.Domain.Person", new List<string>()));
             expectedFields.Add(new NameAndType("PersonId", "System.Int32", new List<string>()));
             expectedFields.Add(new NameAndType("RequestedAt", "System.DateTime", new List<string>()));
             expectedFields.Add(new NameAndType("RequestedBy", "Keas.Core.Domain.User", new List<string>()));
             expectedFields.Add(new NameAndType("RequestedByName", "System.String", new List<string>()));
-
             #endregion Arrange
 
             AttributeAndFieldValidation.ValidateFieldsAndAttributes(expectedFields, typeof(KeySerialAssignment));
