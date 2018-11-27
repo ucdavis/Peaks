491be90ad64dbbc95ed69a2fd3d6c9cbfd9181fd
John Knoll <jpknoll@gmail.com>
2018-11-27 11:10:45 -0800

    use assetbase

--- Keas.Core/Domain/KeySerial.cs
+++ Keas.Core/Domain/KeySerial.cs
@@ -1,12 +1,9 @@
 ﻿using System;
-using System.Collections.Generic;
-using System.ComponentModel.DataAnnotations;
-using System.Text;
 using Microsoft.EntityFrameworkCore;
 
 namespace Keas.Core.Domain
 {
-    public class KeySerial
+    public class KeySerial : AssetBase
     {
 
         public KeySerial()
@@ -14,19 +11,17 @@ namespace Keas.Core.Domain
             Active = true;
         }
 
-        [Key]
-        public int Id { get; set; }
+        public string Number { get; set; }
 
         public Key Key { get; set; }
-        public int KeyId { get; set; }
 
-        public string Number { get; set; }
+        public int KeyId { get; set; }
 
         public KeySerialAssignment KeySerialAssignment { get; set; }
 
         public int? KeySerialAssignmentId { get; set; }
 
-        public bool Active { get; set; }
+        public override string Title => Name ?? Number;
 
         protected internal static void OnModelCreating(ModelBuilder builder)
         {
