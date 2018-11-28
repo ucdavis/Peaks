cc3a927eb931d2b372dbb2543a9b19bc92befbee
John Knoll <jpknoll@gmail.com>
2018-11-28 12:21:35 -0800

    code style

--- Keas.Mvc/ClientApp/components/Keys/AssociateSpace.tsx
+++ Keas.Mvc/ClientApp/components/Keys/AssociateSpace.tsx
@@ -1,11 +1,6 @@
 import * as PropTypes from "prop-types";
 import * as React from "react";
-import {
-  Button,
-  Modal,
-  ModalBody,
-  ModalFooter,
-} from "reactstrap";
+import { Button, Modal, ModalBody, ModalFooter } from "reactstrap";
 
 import { AppContext, IKey, ISpace } from "../../Types";
 
@@ -13,88 +8,91 @@ import KeyEditValues from "./KeyEditValues";
 import SearchKeys from "./SearchKeys";
 import SearchSpaces from "../Spaces/SearchSpaces";
 
-
 interface IProps {
-  onAssign: (space: ISpace, key: IKey) => void;
-  openModal: () => void;
-  closeModal: () => void;
-  
-  isModalOpen: boolean;
-  searchableTags: string[];
+    onAssign: (space: ISpace, key: IKey) => void;
+    openModal: () => void;
+    closeModal: () => void;
 
-  selectedKey?: IKey;
-  selectedSpace?: ISpace;
+    isModalOpen: boolean;
+    searchableTags: string[];
+
+    selectedKey?: IKey;
+    selectedSpace?: ISpace;
 }
 
 interface IState {
-  error: string;
-  selectedKey: IKey;
-  selectedSpace: ISpace;
-  submitting: boolean;
-  validState: boolean;
+    error: string;
+    selectedKey: IKey;
+    selectedSpace: ISpace;
+    submitting: boolean;
+    validState: boolean;
 }
 
 export default class AssociateSpace extends React.Component<IProps, IState> {
-  public static contextTypes = {
-    fetch: PropTypes.func,
-    team: PropTypes.object
-  };
-
-  public context: AppContext;
-
-  constructor(props: IProps) {
-    super(props);
-
-    this.state = {
-      error: "",
-      selectedKey: this.props.selectedKey,
-      selectedSpace: this.props.selectedSpace,
-      submitting: false,
-      validState: false
+    public static contextTypes = {
+        fetch: PropTypes.func,
+        team: PropTypes.object
     };
-  }
 
-  public componentWillReceiveProps(nextProps: IProps) {
-    if (nextProps.selectedKey !== this.props.selectedKey) {
-      this.setState({ selectedKey: nextProps.selectedKey });
+    public context: AppContext;
+
+    constructor(props: IProps) {
+        super(props);
+
+        this.state = {
+            error: "",
+            selectedKey: this.props.selectedKey,
+            selectedSpace: this.props.selectedSpace,
+            submitting: false,
+            validState: false
+        };
     }
 
-    if (nextProps.selectedSpace !== this.props.selectedSpace) {
-      this.setState({ selectedSpace: nextProps.selectedSpace });
-    }
-  }
+    public componentWillReceiveProps(nextProps: IProps) {
+        if (nextProps.selectedKey !== this.props.selectedKey) {
+            this.setState({ selectedKey: nextProps.selectedKey });
+        }
 
-  public render() {
-    return (
-      <div>
-        <Button color="link" onClick={this.props.openModal}>
+        if (nextProps.selectedSpace !== this.props.selectedSpace) {
+            this.setState({ selectedSpace: nextProps.selectedSpace });
+        }
+    }
+
+    public render() {
+        return (
+            <div>
+                <Button color="link" onClick={this.props.openModal}>
                     <i className="fas fa-plus fa-sm mr-2" aria-hidden="true" />
                     Associate
-        </Button>
-        { this.renderModal() }
-      </div>
-    );
-  }
-  
-  private renderModal() {
+                </Button>
+                {this.renderModal()}
+            </div>
+        );
+    }
 
-    const { isModalOpen, selectedKey, selectedSpace } = this.props;
-    const { validState, submitting } = this.state;
+    private renderModal() {
+        const { isModalOpen, selectedKey, selectedSpace } = this.props;
+        const { validState, submitting } = this.state;
 
-    return (
-      <Modal isOpen={isModalOpen} toggle={this._closeModal} size="lg" className="keys-color">
-        <div className="modal-header row justify-content-between">
-          <h2>Assign Key</h2>
-          <Button color="link" onClick={this._closeModal}>
-          <i className="fas fa-times fa-lg"/>
-          </Button>
-        </div>
-        <ModalBody>
-          <div className="form-group">
-            <label htmlFor="assignto">Associate With</label>
-            <SearchSpaces
-              defaultSpace={selectedSpace}
-              onSelect={this._onSelectSpace}
+        return (
+            <Modal
+                isOpen={isModalOpen}
+                toggle={this._closeModal}
+                size="lg"
+                className="keys-color"
+            >
+                <div className="modal-header row justify-content-between">
+                    <h2>Assign Key</h2>
+                    <Button color="link" onClick={this._closeModal}>
+                        <i className="fas fa-times fa-lg" />
+                    </Button>
+                </div>
+                <ModalBody>
+                    <div className="form-group">
+                        <label htmlFor="assignto">Associate With</label>
+                        <SearchSpaces
+                            defaultSpace={selectedSpace}
+                            onSelect={this._onSelectSpace}
                         />
                     </div>
 
@@ -106,7 +104,7 @@ export default class AssociateSpace extends React.Component<IProps, IState> {
                 </ModalBody>
                 <ModalFooter>
                     <Button
-            color="primary"
+                        color="primary"
                         onClick={this._assignSelected}
                         disabled={!validState || submitting}
                     >
@@ -118,7 +116,7 @@ export default class AssociateSpace extends React.Component<IProps, IState> {
                 </ModalFooter>
             </Modal>
         );
-  }
+    }
 
     private renderSearchKey() {
         const { selectedKey } = this.props;
@@ -152,80 +150,82 @@ export default class AssociateSpace extends React.Component<IProps, IState> {
         const { searchableTags } = this.props;
         const { selectedKey } = this.state;
 
-    if (!selectedKey || selectedKey.id > 0) {
-      return;
+        if (!selectedKey || selectedKey.id > 0) {
+            return;
+        }
+
+        return (
+            <KeyEditValues
+                selectedKey={selectedKey}
+                changeProperty={this._changeProperty}
+                disableEditing={false}
+                searchableTags={searchableTags}
+            />
+        );
     }
 
-    return (
-      <KeyEditValues
-        selectedKey={selectedKey}
-        changeProperty={this._changeProperty}
-        disableEditing={false}
-        searchableTags={searchableTags}
-      />
-    );
-  }
+    private _changeProperty = (property: string, value: string) => {
+        this.setState(
+            {
+                selectedKey: {
+                    ...this.state.selectedKey,
+                    [property]: value
+                }
+            },
+            this._validateState
+        );
+    };
 
-  private _changeProperty = (property: string, value: string) => {
-    this.setState({
-      selectedKey: {
-        ...this.state.selectedKey,
-        [property]: value
-      }
-    }, this._validateState);
-  };
+    // default everything out on close
+    private _closeModal = () => {
+        const { selectedKey, selectedSpace } = this.props;
 
-  // default everything out on close
-  private _closeModal = () => {
-    const { selectedKey, selectedSpace } = this.props;
-    
-    this.setState({
-      error: "",
-      selectedKey,
-      selectedSpace,
-      submitting: false,
-      validState: false
-    });
-    
-    this.props.closeModal();
-  }
+        this.setState({
+            error: "",
+            selectedKey,
+            selectedSpace,
+            submitting: false,
+            validState: false
+        });
 
-  // assign the selected key even if we have to create it
-  private _assignSelected = async () => {
-    const { selectedSpace, selectedKey } = this.state;
+        this.props.closeModal();
+    };
 
-    if (!this.state.validState || this.state.submitting) {
-      return;
-    }
+    // assign the selected key even if we have to create it
+    private _assignSelected = async () => {
+        const { selectedSpace, selectedKey } = this.state;
 
-    this.setState({ submitting: true });
+        if (!this.state.validState || this.state.submitting) {
+            return;
+        }
 
-    await this.props.onAssign(selectedSpace, selectedKey);
+        this.setState({ submitting: true });
 
-    this._closeModal();
-  }
+        await this.props.onAssign(selectedSpace, selectedKey);
 
-  private _onSelectedKey = (key: IKey) => {
-    this.setState({ selectedKey: key, error: "" }, this._validateState);
-  }
+        this._closeModal();
+    };
 
-  private _onDeselected = () => {
-    this.setState({ selectedKey: null, error: "" }, this._validateState);
-  }
+    private _onSelectedKey = (key: IKey) => {
+        this.setState({ selectedKey: key, error: "" }, this._validateState);
+    };
 
-  private _onSelectSpace = (space: ISpace) => {
-    this.setState({ selectedSpace: space }, this._validateState);
-  }
+    private _onDeselected = () => {
+        this.setState({ selectedKey: null, error: "" }, this._validateState);
+    };
 
-  private _validateState = () => {
-    let valid = true;
-    if (!this.state.selectedKey) {
-      valid = false;
-    }
-    else if (this.state.error !== "") {
-      valid = false;
-    }
-    
-    this.setState({ validState: valid });
-  }
+    private _onSelectSpace = (space: ISpace) => {
+        this.setState({ selectedSpace: space }, this._validateState);
+    };
+
+    private _validateState = () => {
+        let valid = true;
+        if (!this.state.selectedKey) {
+            valid = false;
+        } else if (this.state.error !== "") {
+            valid = false;
+        }
+
+        this.setState({ validState: valid });
+    };
 }
