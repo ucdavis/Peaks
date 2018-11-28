4692bcb3e56a3d44ef8c360cff12f9cf3481ff6e
John Knoll <jpknoll@gmail.com>
2018-11-28 14:14:41 -0800

    format

--- Keas.Mvc/ClientApp/components/Keys/KeySerialListItem.tsx
+++ Keas.Mvc/ClientApp/components/Keys/KeySerialListItem.tsx
@@ -5,42 +5,57 @@ import { IKeySerial } from "../../Types";
 import ListActionsDropdown, { IAction } from "../ListActionsDropdown";
 
 interface IProps {
-  keySerial: IKeySerial;
-  onRevoke?: (key: IKeySerial) => void;
-  onAssign?: (key: IKeySerial) => void;
-  onUpdate?: (key: IKeySerial) => void;
-  showDetails?: (key: IKeySerial) => void;
-  onEdit?: (key: IKeySerial) => void;
+    keySerial: IKeySerial;
+    onRevoke?: (key: IKeySerial) => void;
+    onAssign?: (key: IKeySerial) => void;
+    onUpdate?: (key: IKeySerial) => void;
+    showDetails?: (key: IKeySerial) => void;
+    onEdit?: (key: IKeySerial) => void;
 }
 
 export default class KeyListItem extends React.Component<IProps, {}> {
-  public render() {
-    const { keySerial } = this.props;
+    public render() {
+        const { keySerial } = this.props;
 
-    const actions: IAction[] = [];
-    if (!!this.props.onAssign && !keySerial.keySerialAssignment) {
-      actions.push({ title: 'Assign', onClick: () => this.props.onAssign(keySerial) });
-    }
+        const actions: IAction[] = [];
+        if (!!this.props.onAssign && !keySerial.keySerialAssignment) {
+            actions.push({
+                title: "Assign",
+                onClick: () => this.props.onAssign(keySerial)
+            });
+        }
 
-    if (!!this.props.onEdit) {
-      actions.push({ title: 'Edit', onClick: () => this.props.onEdit(keySerial) });
-    }
-    
-    if (!!this.props.showDetails) {
-        actions.push({ title: 'Details', onClick: () => this.props.showDetails(keySerial) });
-    }
+        if (!!this.props.onEdit) {
+            actions.push({
+                title: "Edit",
+                onClick: () => this.props.onEdit(keySerial)
+            });
+        }
 
-    if (!!this.props.onUpdate && !!keySerial.keySerialAssignment) {
-      actions.push({ title: 'Update', onClick: () => this.props.onUpdate(keySerial) });
-    }
+        if (!!this.props.showDetails) {
+            actions.push({
+                title: "Details",
+                onClick: () => this.props.showDetails(keySerial)
+            });
+        }
 
-    if (!!this.props.onRevoke && !!keySerial.keySerialAssignment) {
-      actions.push({ title: 'Revoke', onClick: () => this.props.onRevoke(keySerial) });
-    }
-    
-    return (
-      <tr>
-        <td>{keySerial.key.code}</td>
+        if (!!this.props.onUpdate && !!keySerial.keySerialAssignment) {
+            actions.push({
+                title: "Update",
+                onClick: () => this.props.onUpdate(keySerial)
+            });
+        }
+
+        if (!!this.props.onRevoke && !!keySerial.keySerialAssignment) {
+            actions.push({
+                title: "Revoke",
+                onClick: () => this.props.onRevoke(keySerial)
+            });
+        }
+
+        return (
+            <tr>
+                <td>{keySerial.key.code}</td>
                 <td>{keySerial.number}</td>
                 <td>
                     <span className="text-mono">{keySerial.status}</span>
@@ -56,7 +71,7 @@ export default class KeyListItem extends React.Component<IProps, {}> {
                 <td>
                     <ListActionsDropdown actions={actions} />
                 </td>
-      </tr>
-    );
-  }
+            </tr>
+        );
+    }
 }
