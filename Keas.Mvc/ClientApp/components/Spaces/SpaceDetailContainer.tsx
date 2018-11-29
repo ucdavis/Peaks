079ecd087642c49e6d646bc1ad500ce30ce509ec
Jason Sylvestre <jsylvestre@ucdavis.edu>
2018-11-29 13:38:56 -0800

    Assignable Area usually

--- Keas.Mvc/ClientApp/components/Spaces/SpaceDetailContainer.tsx
+++ Keas.Mvc/ClientApp/components/Spaces/SpaceDetailContainer.tsx
@@ -15,6 +15,7 @@ export default class SpacesDetailContainer extends React.Component<IProps, {}> {
         <div>
           <h2 className="card-title">{this.props.space.roomNumber} {this.props.space.bldgName}</h2>
           <div className="card-text">
+            {this.props.space.roomCategoryName && <div>{this.props.space.roomCategoryName}</div>}
             {this.props.space.roomName && <div>{this.props.space.roomName}</div>}
             {this.props.space.floorName && <div>{this.props.space.floorName}</div>}
             {this.props.space.sqFt && <div>{this.props.space.sqFt} Sq Feet</div>}
