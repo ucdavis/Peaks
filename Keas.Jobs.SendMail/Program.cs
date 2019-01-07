f6f07f87d8214b90f4f6d826fee74a6efcabcce8
John Knoll <jpknoll@gmail.com>
2019-01-07 13:42:05 -0800

    use log statements

--- Keas.Jobs.SendMail/Program.cs
+++ Keas.Jobs.SendMail/Program.cs
@@ -1,4 +1,4 @@
-﻿
+
 using System;
 using System.IO;
 using System.Linq;
@@ -10,64 +10,44 @@ using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 
 using Keas.Core.Services;
+using Keas.Jobs.Core;
+using Serilog;
 
 namespace Keas.Jobs.SendMail
 {
-    public class Program
+    public class Program : JobBase
     {
-        public static IConfigurationRoot Configuration { get; set; }
-        public static IServiceProvider Provider { get; set; }
+        private static ILogger _log;
 
-        public static IEmailService EmailService { get; set; }
         static void Main(string[] args)
         {
-            Console.WriteLine("Beginning Email Job!");
+            Configure();
 
-            // Use this to get configuration info, environmental comes in from azure
-            var builder = new ConfigurationBuilder()
-               .SetBasePath(Directory.GetCurrentDirectory())
-               .AddJsonFile("appsettings.json");
-            var environmentName = Environment.GetEnvironmentVariable("ASPNETCORE_ENVIRONMENT");
-            if (string.Equals(environmentName, "development", StringComparison.OrdinalIgnoreCase))
-            {
-                builder.AddUserSecrets<Program>();
-            }
+            var assembyName = typeof(Program).Assembly.GetName();
+            _log = Log.Logger
+                .ForContext("jobname", assembyName.Name)
+                .ForContext("jobid", Guid.NewGuid());
 
-            builder.AddEnvironmentVariables();
-            Configuration = builder.Build();
+            _log.Information("Running {job} build {build}", assembyName.Name, assembyName.Version);
 
-            //TODO: Logging
-            //LogHelper.ConfigureLogging(Configuration);
-            //var assembyName = typeof(Program).Assembly.GetName();
-            //Log.Information("Running {job} build {build}", assembyName.Name, assembyName.Version);
-
-            IServiceCollection services = new ServiceCollection();
-            services.AddOptions();
-            services.AddDbContextPool<ApplicationDbContext>(o => o.UseSqlServer(Configuration.GetConnectionString("DefaultConnection")));
-            
-
-
-
-            services.AddTransient<IEmailService, EmailService>();
-            services.Configure<EmailSettings>(Configuration.GetSection("Email"));
-
-            Provider = services.BuildServiceProvider();
-            EmailService = Provider.GetService<IEmailService>();
+            // setup di
+            var provider = ConfigureServices();
+            var dbContext = provider.GetService<ApplicationDbContext>();
+            var emailService = provider.GetService<IEmailService>();
 
             //TODO: db stuff 
-            var dbContext = Provider.GetService<ApplicationDbContext>();
             var counter = 0;
             var usersWithPendingNotifications = dbContext.Notifications.Where(a => a.Pending).Select(s => s.User).Distinct().ToArray();
             if (usersWithPendingNotifications.Any())
             {
                 foreach (var user in usersWithPendingNotifications)
                 {
-                    EmailService.SendNotificationMessage(user).GetAwaiter().GetResult(); //TODO: Pass param?
+                    emailService.SendNotificationMessage(user).GetAwaiter().GetResult(); //TODO: Pass param?
                     counter++;
                 }
             }
-            Console.WriteLine($"Notifications Sent {counter}");
-            Console.WriteLine("Staring Expiry Notifications");
+            _log.Information("Notifications Sent {counter}", counter);
+            _log.Information("Staring Expiry Notifications");
 
             // Email persons with expiring items, cc teammembers as needed
             counter = 0;
@@ -81,20 +61,32 @@ namespace Keas.Jobs.SendMail
             var personIds = expiringItems.GetPersonIdList();
 
             expiringItems.People = dbContext.People.Where(a => personIds.Contains(a.Id)).Include(a => a.Team).AsNoTracking().ToListAsync().GetAwaiter().GetResult();
-            Console.WriteLine($"About to write {personIds.Count} Expiry Emails");
+            _log.Information("About to write {count} Expiry Emails", personIds.Count);
 
             if (personIds.Any())
             {
                 foreach (var personId in personIds)
                 {
                     // TODO: build new service method to handle these emails
-                    EmailService.SendExpiringMessage(personId, expiringItems).GetAwaiter().GetResult(); //TODO: Pass param?
+                    emailService.SendExpiringMessage(personId, expiringItems).GetAwaiter().GetResult(); //TODO: Pass param?
                     counter++;
                 }
             }
-            Console.WriteLine($"Expiring Sent {counter}");
+            _log.Information("Expiring Sent {count}", counter);
 
-            Console.WriteLine("Done Email Job");
+            _log.Information("Done Email Job");
+        }
+
+        private static ServiceProvider ConfigureServices()
+        {
+            IServiceCollection services = new ServiceCollection();
+            services.AddOptions();
+            services.AddDbContextPool<ApplicationDbContext>(o => o.UseSqlServer(Configuration.GetConnectionString("DefaultConnection")));
+
+            services.AddTransient<IEmailService, EmailService>();
+            services.Configure<EmailSettings>(Configuration.GetSection("Email"));
+
+            return services.BuildServiceProvider();
         }
     }
 }
