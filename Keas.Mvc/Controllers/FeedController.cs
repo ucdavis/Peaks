4a658b0999f3cf6736461399b695cdc68ec44eaa
James Cubbage <jscubbage@ucdavis.edu>
2019-01-17 16:18:06 -0800

    Split. Add API code check as unauth.

--- Keas.Mvc/Controllers/FeedController.cs
+++ Keas.Mvc/Controllers/FeedController.cs
@@ -11,6 +11,7 @@ using Keas.Core.Domain;
 
 namespace Keas.Mvc.Controllers
 {
+    [AllowAnonymous]
     public class FeedController : SuperController
     {
          private readonly ApplicationDbContext _context;
@@ -19,27 +20,48 @@ namespace Keas.Mvc.Controllers
         {
             this._context = context;
         }
-        public async Task<IActionResult> TeamFeed(Guid id, string space)
+        public async Task<IActionResult> TeamFeed(Guid id, string includeSpace)
         {            
-            if(space == "yes")
+             var validKey = await _context.Teams.Where(t => t.Slug == Team && t.ApiCode == id).AnyAsync();
+
+            if(!validKey){
+                Unauthorized();
+            }   
+
+            if(includeSpace == "yes")
             {
-                var people = await _context.People
-                .Where(x=> x.Team.Slug == Team && x.Active && x.Team.ApiCode == id)
+                var people = GetPeopleFeedIncludeSpace();
+                return Json(people);
+            } else
+            {
+                var people = GetPeopleFeed();
+                return Json(people);
+            }  
+        } 
+
+        private async Task<Array> GetPeopleFeed()
+        {
+            var people = await _context.People
+                .Where(x=> x.Team.Slug == Team && x.Active)
+                .Select(p => new {p.Name, p.FirstName, p.LastName, p.Email, p.UserId, p.Title, p.TeamPhone, p.Tags})                
+                .ToArrayAsync();
+
+                return people;
+        }
+
+        private async Task<Array> GetPeopleFeedIncludeSpace()
+        {
+            var people = await _context.People
+                .Where(x=> x.Team.Slug == Team && x.Active)
                 .Select(p => new {p.Name, p.FirstName, p.LastName, p.Email, p.UserId, p.Title, p.TeamPhone, p.Tags,
                 workstation = (from w in _context.Workstations where w.Assignment.PersonId == p.Id select w)
                     .Include(w => w.Space)
                     .Select(w => new {w.Name, w.Space.BldgName, w.Space.RoomNumber})
                 })
-                .ToListAsync();
-                return Json(people);
-            } else
-            {
-                var people = await _context.People
-                .Where(x=> x.Team.Slug == Team && x.Active && x.Team.ApiCode == id)
-                .Select(p => new {p.Name, p.FirstName, p.LastName, p.Email, p.UserId, p.Title, p.TeamPhone, p.Tags})
-                .ToListAsync();
-                return Json(people);
-            }  
-        } 
+                .ToArrayAsync();
+
+                return people;
+        }
+
     }
 }
