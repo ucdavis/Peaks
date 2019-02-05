c36853c899d16091f20640bfe392197477a0e167
James Cubbage <jscubbage@ucdavis.edu>
2019-02-05 13:44:49 -0800

    Don't recreate keys/serials if they exist. Just make active.

--- Keas.Mvc/Controllers/TeamAdminController.cs
+++ Keas.Mvc/Controllers/TeamAdminController.cs
@@ -424,6 +424,10 @@ namespace Keas.Mvc.Controllers
                             {
                                 warning.Append(String.Format("Could not save key in line {0} | ", rowNumber));
                             }
+                        } 
+                        else
+                        {
+                            key.Active = true;
                         }
 
                         if (!string.IsNullOrWhiteSpace(r.SerialNumber))
@@ -460,6 +464,10 @@ namespace Keas.Mvc.Controllers
                                     warning.Append(String.Format("Could not save serial in line {0} | ", rowNumber));
                                 }
 
+                            }
+                            else
+                            {
+                                serial.Active = true;
                             }
 
                             if (!string.IsNullOrWhiteSpace(r.KerbUser))
