4f714f02c6f959572146d4d74a80869ca6f98f04
James Cubbage <jscubbage@ucdavis.edu>
2019-02-11 14:46:42 -0800

    Update after design9 conflict

--- Keas.Mvc/ClientApp/components/People/BioContainer.tsx
+++ Keas.Mvc/ClientApp/components/People/BioContainer.tsx
@@ -23,76 +23,76 @@ export default class BioContainer extends React.Component<IProps, {}> {
                         this.props.person.startDate ||
                         this.props.person.endDate ||
                         this.props.person.supervisor) && (
-                        <div className="person-col">
-                            <h4>Employment Details</h4>
-                            <div className="row justify-content-between">
-                                <div className="person-label-list">
-                                    {this.props.person.title && (
-                                        <p className="person-label">Title</p>
-                                    )}
-                                    {this.props.person.category && <p>Category</p>}
-                                    {this.props.person.startDate && (
-                                        <p className="person-label">Start Date</p>
-                                    )}
-                                    {this.props.person.endDate && (
-                                        <p className="person-label">End Date</p>
-                                    )}
-                                    {this.props.person.supervisor && (
-                                        <p className="person-label">Supervisor</p>
-                                    )}
-                                </div>
-                                <div>
-                                    {this.props.person.title && <p>{this.props.person.title}</p>}
-                                    {this.props.person.category && (
-                                        <p>{this.props.person.category}</p>
-                                    )}
-                                    {this.props.person.startDate && (
-                                        <p>
-                                            {DateUtil.formatExpiration(this.props.person.startDate)}
-                                        </p>
-                                    )}
+                            <div className="person-col">
+                                <h4>Employment Details</h4>
+                                <div className="row justify-content-between">
+                                    <div className="person-label-list">
+                                        {this.props.person.title && (
+                                            <p className="person-label">Title</p>
+                                        )}
+                                        {this.props.person.category && <p>Category</p>}
+                                        {this.props.person.startDate && (
+                                            <p className="person-label">Start Date</p>
+                                        )}
+                                        {this.props.person.endDate && (
+                                            <p className="person-label">End Date</p>
+                                        )}
+                                        {this.props.person.supervisor && (
+                                            <p className="person-label">Supervisor</p>
+                                        )}
+                                    </div>
+                                    <div>
+                                        {this.props.person.title && <p>{this.props.person.title}</p>}
+                                        {this.props.person.category && (
+                                            <p>{this.props.person.category}</p>
+                                        )}
+                                        {this.props.person.startDate && (
+                                            <p>
+                                                {DateUtil.formatExpiration(this.props.person.startDate)}
+                                            </p>
+                                        )}
 
-                                    {this.props.person.endDate && (
-                                        <p>
-                                            {DateUtil.formatExpiration(this.props.person.endDate)}
-                                        </p>
-                                    )}
-                                    {this.props.person.supervisor && (
-                                        <p>{this.props.person.supervisor.name}</p>
-                                    )}
+                                        {this.props.person.endDate && (
+                                            <p>
+                                                {DateUtil.formatExpiration(this.props.person.endDate)}
+                                            </p>
+                                        )}
+                                        {this.props.person.supervisor && (
+                                            <p>{this.props.person.supervisor.name}</p>
+                                        )}
+                                    </div>
                                 </div>
                             </div>
-                        </div>
-                    )}
+                        )}
                     {(this.props.person.email ||
                         this.props.person.homePhone ||
                         this.props.person.teamPhone) && (
-                        <div className="person-col">
-                            <h4>Contact Details</h4>
-                            <div className="row justify-content-between">
-                                <div className="person-label-list">
-                                    {this.props.person.email && (
-                                        <p className="person-label">Email</p>
-                                    )}
-                                    {this.props.person.homePhone && (
-                                        <p className="person-label">Home</p>
-                                    )}
-                                    {this.props.person.teamPhone && (
-                                        <p className="person-label">Team</p>
-                                    )}
-                                </div>
-                                <div>
-                                    {this.props.person.email && <p>{this.props.person.email}</p>}
-                                    {this.props.person.homePhone && (
-                                        <p>{this.props.person.homePhone}</p>
-                                    )}
-                                    {this.props.person.teamPhone && (
-                                        <p>{this.props.person.teamPhone}</p>
-                                    )}
+                            <div className="person-col">
+                                <h4>Contact Details</h4>
+                                <div className="row justify-content-between">
+                                    <div className="person-label-list">
+                                        {this.props.person.email && (
+                                            <p className="person-label">Email</p>
+                                        )}
+                                        {this.props.person.homePhone && (
+                                            <p className="person-label">Home</p>
+                                        )}
+                                        {this.props.person.teamPhone && (
+                                            <p className="person-label">Team</p>
+                                        )}
+                                    </div>
+                                    <div>
+                                        {this.props.person.email && <p>{this.props.person.email}</p>}
+                                        {this.props.person.homePhone && (
+                                            <p>{this.props.person.homePhone}</p>
+                                        )}
+                                        {this.props.person.teamPhone && (
+                                            <p>{this.props.person.teamPhone}</p>
+                                        )}
+                                    </div>
                                 </div>
                             </div>
-                        </div>
-                    )}
+                        )}
                     {this.props.person.tags && (
                         <div className="person-col">
                             <h4>Tags</h4>
@@ -100,6 +100,24 @@ export default class BioContainer extends React.Component<IProps, {}> {
                         </div>
                     )}
                 </div>
+                {this.props.person.isSupervisor && (
+                    <div className="person-col">
+                        <h4>Supervisor</h4>
+                        <p><a
+                            href={`/${
+                                this.context.team.slug
+                                }/Report/SupervisorDirectReports/?personId=${
+                                this.props.person.id
+                                }`}
+                            target="_blank" >
+                            <Button className="btn btn-link">
+                                <i className="fas fa-search fa-sm fa-fw mr-2" aria-hidden="true" />
+                                See who this person supervises
+                            </Button>
+                            </a>
+                        </p>
+                    </div>
+                )}            
                 {this.props.person.notes && (
                     <div className="notes-deets">
                         <h4>Notes</h4>
