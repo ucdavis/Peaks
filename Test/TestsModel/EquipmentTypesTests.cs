32524c1445dbaee30ec7a7f85bf38a2cb2ff7057
Jason Sylvestre <jsylvestre@ucdavis.edu>
2019-03-04 10:16:58 -0800

    Add tests

---
+++ Test/TestsModel/EquipmentTypesTests.cs
@@ -0,0 +1,70 @@
+using System;
+using System.Collections.Generic;
+using System.Text;
+using Keas.Core.Models;
+using Shouldly;
+using Xunit;
+
+namespace Test.TestsModel
+{
+    [Trait("Category", "ModelTests")]
+    public class EquipmentTypesTests
+    {
+        [Fact]
+        public void EquipmentTypesListContainsExpectedNumberOfValues()
+        {
+            EquipmentTypes.Types.Count.ShouldBe(6);
+        }
+
+        //1
+        [Fact]
+        public void EquipmentTypesContainsDefault()
+        {
+            EquipmentTypes.Default.ShouldBe("Default");
+            EquipmentTypes.Types.ShouldContain(EquipmentTypes.Default);
+        }
+
+        //2
+        [Fact]
+        public void EquipmentTypesContainsCard()
+        {
+            EquipmentTypes.Card.ShouldBe("Card");
+            EquipmentTypes.Types.ShouldContain(EquipmentTypes.Card);
+        }
+
+        //3
+        [Fact]
+        public void EquipmentTypesContainsComputer()
+        {
+            EquipmentTypes.Computer.ShouldBe("Computer");
+            EquipmentTypes.Types.ShouldContain(EquipmentTypes.Computer);
+        }
+
+        //4
+        [Fact]
+        public void EquipmentTypesContainsDevice()
+        {
+            EquipmentTypes.Device.ShouldBe("Device");
+            EquipmentTypes.Types.ShouldContain(EquipmentTypes.Device);
+        }
+
+
+        //5
+        [Fact]
+        public void EquipmentTypesContainsIndustrial()
+        {
+            EquipmentTypes.Industrial.ShouldBe("Industrial");
+            EquipmentTypes.Types.ShouldContain(EquipmentTypes.Industrial);
+        }
+
+        //6
+        [Fact]
+        public void EquipmentTypesContainsOther()
+        {
+            EquipmentTypes.Other.ShouldBe("Other");
+            EquipmentTypes.Types.ShouldContain(EquipmentTypes.Other);
+        }
+
+
+    }
+}
