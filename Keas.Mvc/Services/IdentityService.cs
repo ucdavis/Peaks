3194caed7b27f255b3771d20b53d5daec123c576
James Cubbage <jscubbage@ucdavis.edu>
2019-03-12 15:55:35 -0700

    How about this

--- Keas.Mvc/Services/IdentityService.cs
+++ Keas.Mvc/Services/IdentityService.cs
@@ -28,7 +28,7 @@ namespace Keas.Mvc.Services
     {
         private readonly AuthSettings _authSettings;
         private readonly ApplicationDbContext _context;
-    
+
 
         public IdentityService(IOptions<AuthSettings> authSettings, ApplicationDbContext context)
         {
@@ -45,11 +45,37 @@ namespace Keas.Mvc.Services
                 user = await GetByKerberos(kerb);
                 if (user != null)
                 {
-                    _context.Users.Add(user);
-                    var person = CreatePersonFromUser(user, teamId);
-                    _context.People.Add(person);
-                    await _context.SaveChangesAsync();
-                    return (person, 1);
+                    try
+                    {
+                        _context.Users.Add(user);
+                        var person = CreatePersonFromUser(user, teamId);
+                        _context.People.Add(person);
+                        await _context.SaveChangesAsync();
+                        return (person, 1);
+                    }
+                    catch (Exception e)
+                    {
+                        var local = _context.Set<User>()
+                            .Local
+                            .FirstOrDefault(entry => entry.Id.Equals(user.Id));
+
+                        // check if local is not null 
+                        if (local != null) // I'm using a extension method
+                        {
+                            // detach
+                            _context.Entry(local).State = EntityState.Detached;
+                        }
+
+                        var localPerson = _context.Set<Person>().Local
+                            .FirstOrDefault(entry => entry.UserId.Equals(user.Id));
+                        if (localPerson != null) // I'm using a extension method
+                        {
+                            // detach
+                            _context.Entry(localPerson).State = EntityState.Detached;
+                        }
+
+                        return (null, 0);
+                    }
                 }
                 else
                 {
@@ -140,7 +166,7 @@ namespace Keas.Mvc.Services
                 var userIDs = ucdKerbResult.ResponseData.Results.Select(a => a.UserId).Distinct().ToArray();
                 if (iamIds.Length != 1 && userIDs.Length != 1)
                 {
-                    throw new Exception($"IAM issue with non unique values for kerbs: {string.Join(',',userIDs)} IAM: {string.Join(',',iamIds)}");
+                    throw new Exception($"IAM issue with non unique values for kerbs: {string.Join(',', userIDs)} IAM: {string.Join(',', iamIds)}");
                 }
             }
 
@@ -199,6 +225,10 @@ namespace Keas.Mvc.Services
                     {
                         var personResult = await GetOrCreatePersonFromKerberos(kerbResults.ResponseData.Results[0].UserId, team.Id);
                         newpeople += personResult.peopleCount;
+                        if (personResult.Person == null)
+                        {
+                            warning.Append($"IAM ID {id.IamId} failed to save.");
+                        }
                     }
                 }
                 else if (!await _context.People.AnyAsync(p => p.User.Iam == id.IamId && p.Team.Slug == teamslug))
