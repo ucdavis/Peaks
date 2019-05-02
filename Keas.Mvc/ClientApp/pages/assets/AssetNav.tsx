062fc2adcbe5265a35570797e1697d4ff5ac80ec
James Cubbage <jscubbage@ucdavis.edu>
2019-05-02 13:38:10 -0700

    Limit viewed links with permissions

--- Keas.Mvc/ClientApp/pages/assets/AssetNav.tsx
+++ Keas.Mvc/ClientApp/pages/assets/AssetNav.tsx
@@ -1,52 +1,58 @@
 import * as PropTypes from 'prop-types';
 import * as React from "react";
-import { match } from "react-router";
-import { Link, NavLink } from "react-router-dom";
+import { NavLink } from "react-router-dom";
+import { PermissionsUtil } from "../../util/permissions";
 
 export default class AssetNav extends React.Component<{}, {}> {
-    public static contextTypes = {
-      team: PropTypes.object
-    };
+  public static contextTypes = {
+    permissions: PropTypes.array,
+    team: PropTypes.object
+  };
   public render() {
     return (
 
       <div>
         <div className="react-tabs">
-        <NavLink
-            to={`/${this.context.team.slug}/people`}
-            className="tab-link tab-people"
-            activeClassName="active"
-          >
-            People
-          </NavLink>
+          {PermissionsUtil.canViewPeople(this.context.permissions) &&
+            <NavLink
+              to={`/${this.context.team.slug}/people`}
+              className="tab-link tab-people"
+              activeClassName="active"
+            >
+              People
+        </NavLink>}
+        {PermissionsUtil.canViewEquipment(this.context.permissions) && 
           <NavLink
             to={`/${this.context.team.slug}/equipment`}
             className="tab-link tab-equipment"
             activeClassName="active"
           >
             Equipment
-          </NavLink>
-          <NavLink
-            to={`/${this.context.team.slug}/access`}
-            className="tab-link tab-access"
-            activeClassName="active"
-          >
-            Access
-          </NavLink>
+          </NavLink>}
+          {PermissionsUtil.canViewAccess(this.context.permissions) &&
+            <NavLink
+              to={`/${this.context.team.slug}/access`}
+              className="tab-link tab-access"
+              activeClassName="active"
+            >
+              Access
+          </NavLink>}
+          {PermissionsUtil.canViewKeys(this.context.permissions) && 
           <NavLink
             to={`/${this.context.team.slug}/keys`}
             className="tab-link tab-keys"
             activeClassName="active"
           >
             Keys
-          </NavLink>
+          </NavLink> }
+          {PermissionsUtil.canViewSpaces(this.context.permissions) && 
           <NavLink
             to={`/${this.context.team.slug}/spaces`}
             className="tab-link tab-spaces"
             activeClassName="active"
           >
             Spaces
-          </NavLink>
+          </NavLink>}
         </div>
 
         <div>{this.props.children}</div>
