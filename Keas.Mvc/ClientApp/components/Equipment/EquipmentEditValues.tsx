97d22a3dccb7200b1356e28853001068ed767cac
Jason Sylvestre <jsylvestre@ucdavis.edu>
2019-05-29 09:50:58 -0700

    Hide make and model if Software

--- Keas.Mvc/ClientApp/components/Equipment/EquipmentEditValues.tsx
+++ Keas.Mvc/ClientApp/components/Equipment/EquipmentEditValues.tsx
@@ -189,13 +189,13 @@ export default class EquipmentEditValues extends React.Component<IProps, {}> {
 
     private _shouldShowForType(type: string, prop: string) {
         if (prop === "Make") {
-            if (type !== "Card") {
+            if (type !== "Card" && type !== "Software") {
                 return true;
             }
             return false;
         }
         if (prop === "Model") {
-            if (type !== "Card") {
+            if (type !== "Card" && type !== "Software") {
                 return true;
             }
             return false;
