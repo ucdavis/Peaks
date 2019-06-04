e2e259f08f614886619774b169ddf1e60354b1e8
Jason Sylvestre <jsylvestre@ucdavis.edu>
2019-06-04 15:24:46 -0700

    Simple access check

--- Keas.Mvc/Controllers/GroupController.cs
+++ Keas.Mvc/Controllers/GroupController.cs
@@ -2,15 +2,32 @@ using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
+using Keas.Core.Data;
 using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
 
 namespace Keas.Mvc.Controllers
 {
     public class GroupController : SuperController
     {
-        public IActionResult Index(int id)
+        private readonly ApplicationDbContext _context;
+
+        public GroupController(ApplicationDbContext context)
         {
-            return View();
+            _context = context;
+        }
+        public async Task<IActionResult> Index(int id)
+        {
+            var group = await _context.Groups.SingleOrDefaultAsync(a =>
+                a.Id == id && a.GroupPermissions.Any(w => w.UserId == User.Identity.Name));
+
+            if (group == null)
+            {
+                ErrorMessage = "Group not found or no access to Group";
+                return RedirectToAction("NoAccess", "Home");
+            }
+
+            return View(group);
         }
     }
 }
