58a2fe1f9c75bc0ee28809f7119c6b36f4bd6d1a
Jason Sylvestre <jsylvestre@ucdavis.edu>
2019-08-14 13:28:26 -0700

    List for MakeAndModelTypes, but not use it (yet)

--- Keas.Core/Models/EquipmentTypes.cs
+++ Keas.Core/Models/EquipmentTypes.cs
@@ -33,6 +33,19 @@ namespace Keas.Core.Models
             Other,
         };
 
+        public static List<string> MakeAndModelTypes = new List<string>
+        {
+            Default,
+            Computer,
+            Desktop,
+            Laptop,
+            Server,
+            Cellphone,
+            Device,
+            Industrial,
+            Other,
+        };
+
         public static List<string> Is3Types = new List<string>
         {
             Computer,
