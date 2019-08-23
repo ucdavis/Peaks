615f2222ca10a2097833c10fba3a2937201d5d65
Jason Sylvestre <jsylvestre@ucdavis.edu>
2019-08-23 09:05:41 -0700

    Identity

--- Keas.Sql/dbo/Tables/AttributeKeys.sql
+++ Keas.Sql/dbo/Tables/AttributeKeys.sql
@@ -1,6 +1,6 @@
 CREATE TABLE [dbo].[AttributeKeys]
 (
-	[Id]			INT				NOT NULL PRIMARY KEY, 
+	[Id]			INT				NOT NULL PRIMARY KEY IDENTITY, 
 	[TeamId]		INT				NULL, 
 	[Key]			NVARCHAR(MAX)	NOT NULL, 
 	[Description]	NVARCHAR(50)	NOT NULL
