3cd869c6688d1c7d95da5a34251bfbe718e27459
Jason Sylvestre <jsylvestre@ucdavis.edu>
2019-08-23 12:28:20 -0700

    tests

---
+++ Test/TestsDatabase/EquipmentAttributeKeyTests.cs
@@ -0,0 +1,62 @@
+using System;
+using System.Collections.Generic;
+using System.Text;
+using Keas.Core.Domain;
+using TestHelpers.Helpers;
+using Xunit;
+using Xunit.Abstractions;
+
+namespace Test.TestsDatabase
+{
+    [Trait("Category","DatabaseTests")]
+    public class EquipmentAttributeKeyTests
+    {
+        private readonly ITestOutputHelper _output;
+
+        public EquipmentAttributeKeyTests(ITestOutputHelper output)
+        {
+            _output = output;
+        }
+
+        #region Reflection of Database
+
+        [Fact]
+        public void TestClassAttributes()
+        {
+            // Arrange
+            var classReflection = new ControllerReflection(_output, typeof(EquipmentAttributeKey));
+            // Act
+            // Assert	
+            classReflection.ControllerInherits("Object"); 
+            classReflection.ClassExpectedNoAttribute();
+        }
+
+        [Fact]
+        public void TestDatabaseFieldAttributes()
+        {
+            #region Arrange
+            var expectedFields = new List<NameAndType>();
+            expectedFields.Add(new NameAndType("Description", "System.String", new List<string>
+            {
+                "[System.ComponentModel.DataAnnotations.RequiredAttribute()]",
+                "[System.ComponentModel.DataAnnotations.StringLengthAttribute((Int32)50)]",
+            }));
+            expectedFields.Add(new NameAndType("Id", "System.Int32", new List<string>()));
+            expectedFields.Add(new NameAndType("Key", "System.String", new List<string>
+            {
+                "[System.ComponentModel.DataAnnotations.RequiredAttribute()]",
+            }));
+            expectedFields.Add(new NameAndType("Team", "Keas.Core.Domain.Team", new List<string>()));
+            expectedFields.Add(new NameAndType("TeamId", "System.Nullable`1[System.Int32]", new List<string>()));
+            
+            
+            
+          
+            #endregion Arrange
+
+            AttributeAndFieldValidation.ValidateFieldsAndAttributes(expectedFields, typeof(EquipmentAttributeKey));
+        }
+
+        #endregion Reflection of Database
+    }
+}
