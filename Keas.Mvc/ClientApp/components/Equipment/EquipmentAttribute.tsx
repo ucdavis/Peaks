045b34e4088234e4b6fd0f9418ab9ae0a223f090
Jason Sylvestre <jsylvestre@ucdavis.edu>
2019-08-30 11:12:17 -0700

    Not found message

--- Keas.Mvc/ClientApp/components/Equipment/EquipmentAttribute.tsx
+++ Keas.Mvc/ClientApp/components/Equipment/EquipmentAttribute.tsx
@@ -69,6 +69,7 @@ export default class EquipmentAttribute extends React.Component<IProps, {}> {
                     }
 
                 }}
+                emptyLabel='Attribute key not found. Your DA can add.'
                 inputProps={{
                     className: style
                 }}
