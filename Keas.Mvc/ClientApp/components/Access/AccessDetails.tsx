444aa09029a8174469d8cd353ccb98f560aac773
Laura Holstege <laholstege@ucdavis.edu>
2019-09-09 11:57:39 -0700

    acccess

--- Keas.Mvc/ClientApp/components/Access/AccessDetails.tsx
+++ Keas.Mvc/ClientApp/components/Access/AccessDetails.tsx
@@ -1,6 +1,7 @@
-﻿import { IAccess, IAccessAssignment } from "ClientApp/Types";
+﻿import * as PropTypes from "prop-types";
 import * as React from "react";
 import { Button, Modal, ModalBody } from "reactstrap";
+import { AppContext, IAccess, IAccessAssignment } from "../../Types";
 import AccessEditValues from "./AccessEditValues";
 
 interface IProps {
@@ -9,9 +10,34 @@ interface IProps {
     selectedAccess: IAccess;
     onRevoke: (accessAssignment: IAccessAssignment) => void;
     openEditModal: (access: IAccess) => void;
+    updateSelectedAccess: (access: IAccess) => void;
 }
 
 export default class AccessDetails extends React.Component<IProps, {}> {
+    public static contextTypes = {
+        fetch: PropTypes.func,
+        team: PropTypes.object
+    };
+    public context: AppContext;
+
+    public componentDidMount() {
+        if (!this.props.selectedAccess) {
+            return;
+        }
+        this._fetchDetails(this.props.selectedAccess.id);
+    }
+
+    // make sure we change the key we are updating if the parent changes selected key
+    public componentWillReceiveProps(nextProps: IProps) {
+        if (
+            nextProps.selectedAccess &&
+            (!this.props.selectedAccess ||
+                nextProps.selectedAccess.id !== this.props.selectedAccess.id)
+        ) {
+            this._fetchDetails(nextProps.selectedAccess.id);
+        }
+    }
+
     public render() {
         if (!this.props.selectedAccess) {
             return null;
@@ -44,4 +70,10 @@ export default class AccessDetails extends React.Component<IProps, {}> {
             </div>
         );
     }
+
+    private _fetchDetails = async (id: number) => {
+        const url = `/api/${this.context.team.slug}/access/details/${id}`;
+        const access = await this.context.fetch(url);
+        this.props.updateSelectedAccess(access);
+    };
 }
