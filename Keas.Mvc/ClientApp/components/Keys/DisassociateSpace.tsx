0cebfd021fca6150b0f5eeb69cb2244092ec51fc
Laura Holstege <laholstege@ucdavis.edu>
2019-09-17 16:27:38 -0700

    add details to modal

--- Keas.Mvc/ClientApp/components/Keys/DisassociateSpace.tsx
+++ Keas.Mvc/ClientApp/components/Keys/DisassociateSpace.tsx
@@ -1,6 +1,8 @@
 import * as React from "react";
 import { Button, Modal, ModalBody, ModalFooter } from "reactstrap";
 import { IKeyInfo, ISpace } from "../../Types";
+import KeyEditValues from "./KeyEditValues";
+import SpacesDetailContainer from "../Spaces/SpaceDetailContainer";
 
 interface IProps {
     onDisassociate: (key: IKeyInfo, space: ISpace) => void;
@@ -31,24 +33,49 @@ export default class DisassociateSpace extends React.Component<IProps, IState> {
 
     private renderModal() {
         const { isModalOpen, selectedKeyInfo, selectedSpace } = this.props;
+        if (!selectedKeyInfo || !selectedSpace) {
+            return;
+        }
         const { submitting } = this.state;
 
         return (
             <Modal isOpen={isModalOpen} toggle={this._closeModal} size="lg" className="keys-color">
                 <div className="modal-header row justify-content-between">
-                    <h2>Disassociate</h2>
+                    <h2>Disassociate Key and Space</h2>
                     <Button color="link" onClick={this._closeModal}>
                         <i className="fas fa-times fa-lg" />
                     </Button>
                 </div>
-                <ModalBody>are you sure you want to disassociate .....</ModalBody>
-                <ModalFooter className="justify-content-between">
+                <ModalBody>
+                    <h2>Key</h2>
+                    <KeyEditValues selectedKey={selectedKeyInfo.key} disableEditing={true} />
+                    <h2>Space</h2>
+                    <div>
+                        <div className="form-group">
+                            <label>Room Number and Building</label>
+                            <input
+                                className="form-control"
+                                disabled={true}
+                                value={selectedSpace.roomNumber + " " + selectedSpace.bldgName}
+                            />
+                        </div>
+                        <div className="form-group">
+                            <label>Room Name</label>
+                            <input
+                                className="form-control"
+                                disabled={true}
+                                value={selectedSpace.roomName ? selectedSpace.roomName : ""}
+                            />
+                        </div>
+                    </div>
+                </ModalBody>
+                <ModalFooter>
                     <Button
                         color="primary"
                         onClick={this._disassociateKeyAndSpace}
                         disabled={submitting}
                     >
-                        Disassociate
+                        Go!
                         {submitting && <i className="fas fa-circle-notch fa-spin ml-2" />}
                     </Button>
                 </ModalFooter>
