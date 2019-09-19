3b272a2a0735332e002191ad6c6f34e07b0c6e9f
NabilFurmoli <nabilfurmoli@gmail.com>
2019-09-19 13:53:54 -0700

    updated the code format

--- Keas.Core/Extensions/StringExtensions.cs
+++ Keas.Core/Extensions/StringExtensions.cs
@@ -4,22 +4,29 @@ using System.Text;
 using Humanizer;
 using PhoneNumbers;
 
-namespace Keas.Core.Extensions {
-    public static class StringExtensions {
-        public static string SafeHumanizeTitle (this string value) {
-            if (string.IsNullOrWhiteSpace (value)) {
+namespace Keas.Core.Extensions
+{
+    public static class StringExtensions
+    {
+        public static string SafeHumanizeTitle(this string value)
+        {
+            if (string.IsNullOrWhiteSpace(value))
+            {
                 return value;
             }
 
-            return value.ToLower ().Humanize (LetterCasing.Title); //Lower it so all caps gets changed
+            return value.ToLower().Humanize(LetterCasing.Title); //Lower it so all caps gets changed
         }
-        public static string FormatPhone (this string value) {
-            if (string.IsNullOrWhiteSpace (value)) {
+        public static string FormatPhone(this string value)
+        {
+            if (string.IsNullOrWhiteSpace(value))
+            {
                 return value;
             }
-            
+
             var phone = "";
-            try {
+            try 
+            {
                 var phoneNumberUtil = PhoneNumbers.PhoneNumberUtil.GetInstance ();
                 var phoneNumber = phoneNumberUtil.ParseAndKeepRawInput (value, "US");
                 phone = phoneNumberUtil.Format (phoneNumber, PhoneNumberFormat.NATIONAL);
@@ -30,19 +37,23 @@ namespace Keas.Core.Extensions {
 
                     return phoneNumber.RawInput;
                 }
-            } catch (Exception) {
+            } 
+            catch (Exception)
+            {
                 return value;
             }
-
+            
             return phone;
         }
 
-        public static string SafeToLower (this string value) {
-            if (value == null) {
+        public static string SafeToLower(this string value)
+        {
+            if (value == null)
+            {
                 return value;
             }
 
-            return value.ToLower ();
+            return value.ToLower();
         }
     }
 }
