f062b6c58dfe962126278fa6fb68396b8dd73eb9
Laura Holstege <laholstege@ucdavis.edu>
2019-10-03 13:12:13 -0700

    prettier

--- Keas.Mvc/ClientApp/logger.ts
+++ Keas.Mvc/ClientApp/logger.ts
@@ -1,12 +1,12 @@
 import {
-    AjaxAppender,
-    BrowserConsoleAppender,
-    getLogger,
-    JsonLayout,
-    Logger,
-    logLog
-} from "log4javascript";
-import * as StackTrace from "stacktrace-js";
+  AjaxAppender,
+  BrowserConsoleAppender,
+  getLogger,
+  JsonLayout,
+  Logger,
+  logLog
+} from 'log4javascript';
+import * as StackTrace from 'stacktrace-js';
 
 const __DEV__: boolean = false;
 
@@ -14,7 +14,7 @@ const logger = getLogger();
 
 // disable user alerts for loglog errors in production
 if (!__DEV__) {
-    logLog.setQuietMode(true);
+  logLog.setQuietMode(true);
 }
 
 // setup console sink
@@ -26,20 +26,20 @@ const host = location.origin || `${location.protocol}//${location.host}`;
 const ajaxSink = new AjaxAppender(`${host}/log/SendLogMessage`, true);
 const ajaxLayout = new JsonLayout(true, true);
 ajaxSink.setLayout(ajaxLayout);
-ajaxSink.addHeader("Content-Type", "application/json");
+ajaxSink.addHeader('Content-Type', 'application/json');
 ajaxSink.setSendAllOnUnload(true);
 logger.addAppender(ajaxSink);
 
 // add global listener
-window.addEventListener("error", async event => {
-    const stack = await StackTrace.fromError(event.error);
-    const message = `${event.message}\r\n${JSON.stringify(stack)}`;
-    logger.error(message, event.error);
+window.addEventListener('error', async event => {
+  const stack = await StackTrace.fromError(event.error);
+  const message = `${event.message}\r\n${JSON.stringify(stack)}`;
+  logger.error(message, event.error);
 });
 
 // add global to window
 export interface ILoggerWindow extends Window {
-    $logger?: Logger;
+  $logger?: Logger;
 }
 
 const loggerWindow: ILoggerWindow = window;
