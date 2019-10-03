9894172f7dd741a1a27ce20b8229247d70535f09
Laura Holstege <laholstege@ucdavis.edu>
2019-10-03 11:03:15 -0700

    move to new context type

---
+++ Keas.Mvc/ClientApp/Context.tsx
@@ -0,0 +1,5 @@
+import * as React from 'react';
+import { AppContext } from './Types';
+
+// tslint:disable-next-line: variable-name
+export const Context = React.createContext<AppContext>(undefined);
