ee62d2e6e95c860d3fefff8d4a300d94d282c393
Laura Holstege <laholstege@ucdavis.edu>
2019-10-03 11:33:24 -0700

    keys

--- Keas.Mvc/ClientApp/components/Keys/CreateKey.tsx
+++ Keas.Mvc/ClientApp/components/Keys/CreateKey.tsx
@@ -1,7 +1,7 @@
-import * as PropTypes from 'prop-types';
 import * as React from 'react';
 import { Button, Modal, ModalBody, ModalFooter } from 'reactstrap';
-import { AppContext, IKey } from '../../Types';
+import { Context } from '../../Context';
+import { IKey } from '../../Types';
 import KeyEditValues from './KeyEditValues';
 
 interface IProps {
@@ -21,12 +21,8 @@ interface IState {
 }
 
 export default class CreateKey extends React.Component<IProps, IState> {
-  public static contextTypes = {
-    fetch: PropTypes.func,
-    team: PropTypes.object
-  };
-
-  public context: AppContext;
+  public static contextType = Context;
+  public context!: React.ContextType<typeof Context>;
 
   private _formRef: React.RefObject<HTMLFormElement>;
 
--- Keas.Mvc/ClientApp/components/Keys/DeleteKey.tsx
+++ Keas.Mvc/ClientApp/components/Keys/DeleteKey.tsx
@@ -1,7 +1,7 @@
-﻿import * as PropTypes from 'prop-types';
-import * as React from 'react';
+﻿import * as React from 'react';
 import { Button, Modal, ModalBody, ModalFooter } from 'reactstrap';
-import { AppContext, IKey, IKeyInfo } from '../../Types';
+import { Context } from '../../Context';
+import { IKey, IKeyInfo } from '../../Types';
 import KeyEditValues from './KeyEditValues';
 
 interface IProps {
@@ -16,11 +16,9 @@ interface IState {
 }
 
 export default class DeleteKey extends React.Component<IProps, IState> {
-  public static contextTypes = {
-    fetch: PropTypes.func,
-    team: PropTypes.object
-  };
-  public context: AppContext;
+  public static contextType = Context;
+  public context!: React.ContextType<typeof Context>;
+
   constructor(props) {
     super(props);
     this.state = {
