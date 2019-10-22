7aaf9917cda8918dccae7774a4496f1995d12ae1
klanmiko <eternity01033@gmail.com>
2019-10-22 12:28:05 -0700

    fix revoke

--- Keas.Mvc/ClientApp/components/Access/RevokeAccess.tsx
+++ Keas.Mvc/ClientApp/components/Access/RevokeAccess.tsx
@@ -1,6 +1,6 @@
 import * as React from 'react';
 import { Alert, Button } from 'reactstrap';
-import {  IAccessAssignment } from '../../Types';
+import { IAccessAssignment } from '../../Types';
 import AccessModal from './AccessModal';
 
 interface IProps {
@@ -11,36 +11,30 @@ interface IProps {
 
 interface IState {
   submitting: boolean;
-  error?: string
+  error?: string;
 }
 
 export default class RevokeAccess extends React.Component<IProps, IState> {
   constructor(props: IProps) {
     super(props);
 
-    this.state = {submitting: false}
+    this.state = { submitting: false };
   }
+
   public render() {
-    const valid = !!this.props.assignment
-    const assignment = this.props.assignment
-    return (
-      valid ? 
+    const valid = !!this.props.assignment;
+    const assignment = this.props.assignment;
+    return valid ? (
       <AccessModal
         isOpen={true}
         closeModal={this.props.cancelRevoke}
-        header={<h1>Revoke Access for {this.props.assignment.person.firstName}</h1>}
+        header={
+          <h1>Revoke Access for {this.props.assignment.person.firstName}</h1>
+        }
         footer={
           <Button
             color='primary'
-            onClick={() => {
-              this.setState({submitting: true})
-              this.props.revoke(this.props.assignment).catch(e => {
-                this.setState({
-                  error: "Error -- " + e.message + " -- Please try again later",
-                  submitting: false,
-                })
-              })
-            }}
+            onClick={this._callRevoke}
             disabled={this.state.submitting}
           >
             Revoke{' '}
@@ -50,19 +44,31 @@ export default class RevokeAccess extends React.Component<IProps, IState> {
           </Button>
         }
       >
-        {this.state.error && (
-          <Alert color="danger">{this.state.error}</Alert>
-        )}
+        {this.state.error && <Alert color='danger'>{this.state.error}</Alert>}
         <h1>Name: </h1>
         <p>{assignment.access.name}</p>
         <h2>Notes: </h2>
         <p>{assignment.access.notes}</p>
         <p>Expires At {assignment.expiresAt}</p>
-      </AccessModal> :  <AccessModal
-      isOpen={true}
-      closeModal={this.props.cancelRevoke}
-      header={<h1>Assignment not found</h1>}
-    />
+      </AccessModal>
+    ) : (
+      <AccessModal
+        isOpen={true}
+        closeModal={this.props.cancelRevoke}
+        header={<h1>Assignment not found</h1>}
+      />
     );
   }
+
+  private _callRevoke = () => {
+    this.setState({ submitting: true });
+    try {
+      this.props.revoke(this.props.assignment)
+    } catch(e) {
+      this.setState({
+        error: 'Error -- ' + e.message + ' -- Please try again later',
+        submitting: false
+      });
+    }
+  };
 }
