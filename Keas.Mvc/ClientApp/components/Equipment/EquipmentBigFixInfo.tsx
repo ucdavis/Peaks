f714d56d0b6dc03802d132989434112204c1695b
Nabil Furmoli <nabilfurmoli@gmail.com>
2019-11-07 13:57:00 -0800

    changed forbiiden error toast output to inline output

--- Keas.Mvc/ClientApp/components/Equipment/EquipmentBigFixInfo.tsx
+++ Keas.Mvc/ClientApp/components/Equipment/EquipmentBigFixInfo.tsx
@@ -13,6 +13,7 @@ interface IState {
   isFetched: boolean;
   isValidRequest: boolean;
   isFound: boolean;
+  isForbidden: boolean;
 }
 
 export default class EquipmentBigFixInfo extends React.Component<
@@ -29,6 +30,7 @@ export default class EquipmentBigFixInfo extends React.Component<
       computerInfo: {},
       isFetched: false,
       isFound: true,
+      isForbidden: false,
       isValidRequest: true
     };
   }
@@ -111,14 +113,17 @@ export default class EquipmentBigFixInfo extends React.Component<
             </tbody>
           </Table>
         );
+      } else if (this.state.isForbidden) {
+        return (
+          <p>Error fetching Computer details due to permissions issue.</p>
+        );
+      } else {
+        return (
+          <p>
+            Not a valid Bigfix id, please make sure to enter a valid Bigfix id.
+          </p>
+        );
       }
-
-      // if not found
-      return (
-        <p>
-          Not a valid Bigfix id, please make sure to enter a valid Bigfix id.
-        </p>
-      );
     }
 
     return <p>No data to present</p>;
@@ -139,11 +144,9 @@ export default class EquipmentBigFixInfo extends React.Component<
       } else if (err.message === 'Forbidden') {
         this.setState({
           isFetched: true,
-          isValidRequest: false
+          isFound: false,
+          isForbidden: true
         });
-        toast.error(
-          'Error fetching Computer details due to a permissions issue.'
-        );
       } else {
         this.setState({
           isFetched: true,
@@ -176,6 +179,7 @@ export default class EquipmentBigFixInfo extends React.Component<
       bigfixModal: !prevState.bigfixModal,
       isFetched: false,
       isFound: true,
+      isForbidden: false,
       isValidRequest: true
     }));
   };
