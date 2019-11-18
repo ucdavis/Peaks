f16115ed2d503f6a95ad6d027b4d02f2d9964764
Laura Holstege <laholstege@ucdavis.edu>
2019-11-18 15:06:51 -0800

    workstation schema

---
+++ Keas.Mvc/ClientApp/models/Workstations.ts
@@ -0,0 +1,41 @@
+import * as yup from 'yup';
+import { IPerson, ISpace } from '../Types';
+
+export interface IWorkstation {
+  id: number;
+  name: string;
+  space: ISpace;
+  tags: string;
+  notes: string;
+  teamId: number;
+  assignment?: IWorkstationAssignment;
+}
+
+export const workstationSchema = yup.object<IWorkstation>().shape({
+  assignment: yup
+    .object<IWorkstationAssignment>()
+    .notRequired()
+    .nullable(),
+  id: yup.number(),
+  name: yup
+    .string()
+    .required()
+    .max(64),
+  notes: yup
+    .string()
+    .notRequired()
+    .nullable(),
+  space: yup.object<ISpace>().required(),
+  tags: yup
+    .string()
+    .notRequired()
+    .nullable(),
+  teamId: yup.number()
+});
+
+export interface IWorkstationAssignment {
+  id: number;
+  workstationId: number;
+  expiresAt: Date;
+  person: IPerson;
+}
