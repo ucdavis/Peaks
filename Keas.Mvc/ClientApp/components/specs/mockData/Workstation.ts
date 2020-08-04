901ac36961c405d0ad4da3526efdd9b5e613f1b9
Bryan M Wong <bmwong@ucdavis.edu>
2020-08-04 14:42:54 -0700

    Updated workstation fakeData

--- Keas.Mvc/ClientApp/components/specs/mockData/Workstation.ts
+++ Keas.Mvc/ClientApp/components/specs/mockData/Workstation.ts
@@ -20,5 +20,65 @@ export const fakeWorkstations: IWorkstation[] = [
     notes: null,
     tags: '',
     teamId: 10
+  },
+  {
+    space: {
+      id: 3541,
+      roomKey: 'DV-01-00007713',
+      bldgName: 'California Hall',
+      floorName: 'First Floor',
+      roomNumber: '001',
+      roomName: 'Something cool',
+      roomCategoryName: 'Assignable Area',
+      orgId: 'AADM',
+      deptName: "Dean's Office, CA&ES",
+      sqFt: '847'
+    },
+    assignment: null,
+    id: 18,
+    name: 'Desk',
+    notes: null,
+    tags: '',
+    teamId: 10
+  },
+  {
+    space: {
+      id: 3542,
+      roomKey: 'DV-01-00007713',
+      bldgName: 'Wall Hall',
+      floorName: 'First Floor',
+      roomNumber: '001',
+      roomName: 'Something cool',
+      roomCategoryName: 'Assignable Area',
+      orgId: 'AADM',
+      deptName: "Dean's Office, CA&ES",
+      sqFt: '847'
+    },
+    assignment: null,
+    id: 19,
+    name: 'Desk',
+    notes: null,
+    tags: '',
+    teamId: 10
+  },
+  {
+    space: {
+      id: 3543,
+      roomKey: 'DV-01-00007713',
+      bldgName: 'Olson Hall',
+      floorName: 'First Floor',
+      roomNumber: '001',
+      roomName: 'Something cool',
+      roomCategoryName: 'Assignable Area',
+      orgId: 'AADM',
+      deptName: "Dean's Office, CA&ES",
+      sqFt: '847'
+    },
+    assignment: null,
+    id: 20,
+    name: 'Desk',
+    notes: null,
+    tags: '',
+    teamId: 10
   }
 ];
