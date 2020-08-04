403a43def5957c268127456d8717b9d9aa698ee3
Bryan M Wong <bmwong@ucdavis.edu>
2020-08-04 16:07:59 -0700

    Finished jest tests for workstations

--- Keas.Mvc/ClientApp/components/Workstations/WorkstationContainer.test.tsx
+++ Keas.Mvc/ClientApp/components/Workstations/WorkstationContainer.test.tsx
@@ -5,6 +5,7 @@ import WorkstationContainer from '../Workstations/WorkstationContainer';
 import { act } from 'react-dom/test-utils';
 import { Context } from '../../Context';
 import { fakeWorkstations } from '../specs/mockData/Workstation';
+import { ISpace } from 'ClientApp/models/Spaces';
 
 // mock all route elements
 let mockRouter: any = {};
@@ -21,6 +22,18 @@ jest.mock('../History/HistoryContainer', () => {
 });
 
 let container: Element = null;
+let selectedSpace: ISpace = {
+  id: 3535,
+  deptName: "Dean's Office, CA&ES",
+  bldgName: 'Mrak Hall',
+  roomKey: 'DV-01-00007674',
+  roomNumber: '0003',
+  roomName: 'Storage',
+  floorName: 'Basement Floor',
+  roomCategoryName: 'Assignable Area',
+  orgId: 'AADM',
+  sqFt: '320'
+};
 
 beforeEach(() => {
   // setup a DOM element as a render target
@@ -55,6 +68,7 @@ describe('Workstation Container', () => {
       render(
         <Context.Provider value={contextObject}>
           <WorkstationContainer
+            space={selectedSpace}
             tags={null}
             history={mockRouter}
             match={mockRouterMatch}
@@ -65,16 +79,140 @@ describe('Workstation Container', () => {
       );
     });
 
-    const headerRecord = document.querySelector('div');
+    const headerRecord = container.querySelector('thead').textContent;
 
-    const consoleSpy = jest.spyOn(console, 'log');
+    expect(headerRecord).toBe('NameSpaceAssigned ToExpirationActions');
+  });
 
-    console.log(headerRecord);
-  
-    expect(consoleSpy).toHaveBeenCalledWith('hello');
+  it('Shows add button', async () => {
+    // await act to make sure pending Promises complete before moving on
+    await act(async () => {
+      // spy on our context's fetch handler to return fake key workstations
+      jest
+        .spyOn(contextObject, 'fetch')
+        .mockImplementation(() => Promise.resolve(fakeWorkstations));
 
-    expect(headerRecord).toBe(
-      ' Key Code and SN 🔼Status 🔼AssignmentExpirationActions'
-    );
+      // important to add the context provider here since it includes permissions and fetch info
+      render(
+        <Context.Provider value={contextObject}>
+          <WorkstationContainer
+            space={selectedSpace}
+            tags={null}
+            history={mockRouter}
+            match={mockRouterMatch}
+            location={mockRouter}
+          />
+        </Context.Provider>,
+        container
+      );
+    });
+
+    const headerRecord = container.querySelector('.card-header-spaces')
+      .textContent;
+    expect(headerRecord).toContain('Add Workstation');
+  });
+
+  it('Shows workstation list', async () => {
+    // await act to make sure pending Promises complete before moving on
+    await act(async () => {
+      // spy on our context's fetch handler to return fake workstations
+      jest
+        .spyOn(contextObject, 'fetch')
+        .mockImplementation(() => Promise.resolve(fakeWorkstations));
+
+      // important to add the context provider here since it includes permissions and fetch info
+      render(
+        <Context.Provider value={contextObject}>
+          <WorkstationContainer
+            space={selectedSpace}
+            tags={null}
+            history={mockRouter}
+            match={mockRouterMatch}
+            location={mockRouter}
+          />
+        </Context.Provider>,
+        container
+      );
+    });
+
+    const matches = container.querySelectorAll('tr');
+
+    let foundIt = false;
+    matches.forEach(function (match) {
+      const rowContent = match.textContent;
+      if (rowContent.includes('Desk0038')) {
+        foundIt = true;
+        expect(rowContent).toContain('Mrak Hall'); // confirm building name is displayed
+      }
+    });
+
+    expect(foundIt).toBeTruthy();
+  });
+
+  it('Shows correct number of workstation', async () => {
+    // await act to make sure pending Promises complete before moving on
+    await act(async () => {
+      // spy on our context's fetch handler to return fake workstations
+      jest
+        .spyOn(contextObject, 'fetch')
+        .mockImplementation(() => Promise.resolve(fakeWorkstations));
+
+      // important to add the context provider here since it includes permissions and fetch info
+      render(
+        <Context.Provider value={contextObject}>
+          <WorkstationContainer
+            space={selectedSpace}
+            tags={null}
+            history={mockRouter}
+            match={mockRouterMatch}
+            location={mockRouter}
+          />
+        </Context.Provider>,
+        container
+      );
+    });
+    const matches = container.querySelectorAll('tbody tr');
+
+    expect(matches.length).toBe(4);
+  });
+
+  it('Shows workstation details', async () => {
+    mockRouter.push = () => {
+      console.log('Nothing');
+    };
+    mockRouterMatch.params = {
+      action: 'details',
+      assetType: 'workstations',
+      id: 17 // test workstation id
+    };
+
+    await act(async () => {
+      // spy on our context's fetch handler to return fake workstations
+      jest
+        .spyOn(contextObject, 'fetch')
+        .mockImplementation(() => Promise.resolve(fakeWorkstations));
+
+      // important to add the context provider here since it includes permissions and fetch info
+      render(
+        <Context.Provider value={contextObject}>
+          <WorkstationContainer
+            space={selectedSpace}
+            tags={null}
+            history={mockRouter}
+            match={mockRouterMatch}
+            location={mockRouter}
+          />
+        </Context.Provider>,
+        container
+      );
+    });
+
+    const detailButton = document.querySelectorAll('button')[1];
+    detailButton.click();
+
+    const details = document.querySelectorAll('input');
+
+    expect(details[0].value).toContain('Desk'); // confirm name is displayed
+    expect(details[1].value).toContain('0038 Mrak Hall'); // confirm room is displayed
   });
 });
