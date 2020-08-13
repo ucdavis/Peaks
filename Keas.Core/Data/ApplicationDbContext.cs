e7dbd009e759442a9f9d426eb4dd4034508768d8
Scott Kirkland <srkirkland@gmail.com>
2020-08-13 15:51:28 -0700

    table to hold team document info

--- Keas.Core/Data/ApplicationDbContext.cs
+++ Keas.Core/Data/ApplicationDbContext.cs
@@ -37,6 +37,7 @@ namespace Keas.Core.Data
         public virtual DbSet<WorkstationAttribute> WorkstationAttributes { get; set; }
 
 		public virtual DbSet<Document> Documents { get; set; }
+		public virtual DbSet<TeamDocumentSetting> TeamDocumentSettings { get; set; }
 
         public virtual DbSet<Role> Roles { get; set; }
         public virtual DbSet<TeamPermission> TeamPermissions { get; set; }
