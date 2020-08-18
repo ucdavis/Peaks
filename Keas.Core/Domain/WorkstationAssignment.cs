f89918bb25661c26fd1b2aa786279dd15c443903
Jason Sylvestre <jsylvestre@ucdavis.edu>
2020-08-18 10:23:30 -0700

    workaround

--- Keas.Core/Domain/WorkstationAssignment.cs
+++ Keas.Core/Domain/WorkstationAssignment.cs
@@ -1,7 +1,7 @@
 namespace Keas.Core.Domain
 {
     public class WorkstationAssignment : AssignmentBase {
-        public int WorkstationId { get; set; }
+        //public int WorkstationId { get; set; } If the ID is added, need to update foreign keys
         public Workstation Workstation { get; set; }
     }
 }
