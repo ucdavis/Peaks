5cdf7bfb212850a3e10c04495f3b3c84c87e2e81
Jason Sylvestre <jsylvestre@ucdavis.edu>
2020-08-19 14:49:15 -0700

    Pulled from keas test db

--- Keas.Sql/dbo/Tables/Documents.sql
+++ Keas.Sql/dbo/Tables/Documents.sql
@@ -1,18 +1,20 @@
-CREATE TABLE [dbo].[Documents] (
-    [Id]                    INT            IDENTITY (1, 1) NOT NULL,
-    [Active]                BIT            NOT NULL,
-    [Name]                  NVARCHAR (64)  NOT NULL,
-    [EnvelopeId]            NVARCHAR (64)  NOT NULL,
-    [TemplateId]            NVARCHAR (64)  NOT NULL,
-    [Status]                NVARCHAR (64)  NOT NULL,
-    [PersonId]              INT            NULL,
-    [Notes]					NVARCHAR(MAX)  NULL, 
-    [Tags]                  NVARCHAR (MAX) NULL,
-    [TeamId]                INT            NOT NULL,
+﻿CREATE TABLE [dbo].[Documents] (
+    [Id]         INT            IDENTITY (1, 1) NOT NULL,
+    [Active]     BIT            NOT NULL,
+    [Name]       NVARCHAR (64)  NOT NULL,
+    [EnvelopeId] NVARCHAR (64)  NOT NULL,
+    [TemplateId] NVARCHAR (64)  NOT NULL,
+    [Status]     NVARCHAR (64)  NOT NULL,
+    [PersonId]   INT            NULL,
+    [Tags]       NVARCHAR (MAX) NULL,
+    [TeamId]     INT            NOT NULL,
+    [Notes]      NVARCHAR (MAX) NULL,
     CONSTRAINT [PK_Documents] PRIMARY KEY CLUSTERED ([Id] ASC),
     CONSTRAINT [FK_Documents_People_PersonId] FOREIGN KEY ([PersonId]) REFERENCES [dbo].[People] ([Id]),
-    CONSTRAINT [FK_Documents_Teams_TeamId] FOREIGN KEY ([TeamId]) REFERENCES [dbo].[Teams] ([Id]) ON DELETE CASCADE  
+    CONSTRAINT [FK_Documents_Teams_TeamId] FOREIGN KEY ([TeamId]) REFERENCES [dbo].[Teams] ([Id]) ON DELETE CASCADE
 );
+
+
 
 GO
 CREATE NONCLUSTERED INDEX [IX_Documents_TeamId]
