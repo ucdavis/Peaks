a6112b5728f3f6c3b8f30f06d057b72b51f72ff2
Bryan M Wong <bmwong@ucdavis.edu>
2020-09-15 15:08:10 -0700

    Converted EditWorkstation and DeleteWorkstation to functional components

--- Keas.Mvc/ClientApp/components/Workstations/DeleteWorkstation.tsx
+++ Keas.Mvc/ClientApp/components/Workstations/DeleteWorkstation.tsx
@@ -1,6 +1,6 @@
 ﻿import * as React from 'react';
+import { useState } from 'react';
 import { Button, Modal, ModalBody, ModalFooter } from 'reactstrap';
-import { Context } from '../../Context';
 import { IWorkstation } from '../../models/Workstations';
 import WorkstationEditValues from './WorkstationEditValues';
 
@@ -11,81 +11,67 @@ interface IProps {
   selectedWorkstation: IWorkstation;
 }
 
-interface IState {
-  submitting: boolean;
-}
+const DeleteWorkstation = (props: IProps) => {
+  const [submitting, setSubmitting] = useState<boolean>(false);
 
-export default class DeleteWorkstation extends React.Component<IProps, IState> {
-  public static contextType = Context;
-  public context!: React.ContextType<typeof Context>;
-
-  constructor(props) {
-    super(props);
-    this.state = {
-      submitting: false
-    };
+  if (!props.selectedWorkstation) {
+    return null;
   }
 
-  public render() {
-    if (!this.props.selectedWorkstation) {
-      return null;
-    }
-    return (
-      <div>
-        <Modal
-          isOpen={this.props.modal}
-          toggle={this.props.closeModal}
-          size='lg'
-          className='workstation-color'
-        >
-          <div className='modal-header row justify-content-between'>
-            <h2>Delete {this.props.selectedWorkstation.name}</h2>
-            <Button color='link' onClick={this.props.closeModal}>
-              <i className='fas fa-times fa-lg' />
-            </Button>
-          </div>
-
-          <ModalBody>
-            <WorkstationEditValues
-              selectedWorkstation={this.props.selectedWorkstation}
-              disableEditing={true}
-              disableSpaceEditing={true}
-            />
-          </ModalBody>
-          <ModalFooter>
-            <Button
-              color='primary'
-              onClick={this._deleteWorkstation}
-              disabled={this.state.submitting}
-            >
-              Go!{' '}
-              {this.state.submitting && (
-                <i className='fas fa-circle-notch fa-spin' />
-              )}
-            </Button>{' '}
-          </ModalFooter>
-        </Modal>
-      </div>
-    );
-  }
-
-  private _deleteWorkstation = async () => {
+  const deleteWorkstation = async () => {
     if (
-      this.props.selectedWorkstation.assignment !== null &&
+      props.selectedWorkstation.assignment !== null &&
       !confirm(
         'This workstation is currently assigned, are you sure you want to delete it?'
       )
     ) {
       return;
     }
-    this.setState({ submitting: true });
+    setSubmitting(true);
     try {
-      await this.props.deleteWorkstation(this.props.selectedWorkstation);
+      await props.deleteWorkstation(props.selectedWorkstation);
     } catch (err) {
-      this.setState({ submitting: false });
+      setSubmitting(false);
       return;
     }
-    this.setState({ submitting: false });
-    this.props.closeModal();
+    setSubmitting(false);
+    props.closeModal();
   };
-}
+
+  return (
+    <div>
+      <Modal
+        isOpen={props.modal}
+        toggle={props.closeModal}
+        size='lg'
+        className='workstation-color'
+      >
+        <div className='modal-header row justify-content-between'>
+          <h2>Delete {props.selectedWorkstation.name}</h2>
+          <Button color='link' onClick={props.closeModal}>
+            <i className='fas fa-times fa-lg' />
+          </Button>
+        </div>
+
+        <ModalBody>
+          <WorkstationEditValues
+            selectedWorkstation={props.selectedWorkstation}
+            disableEditing={true}
+            disableSpaceEditing={true}
+          />
+        </ModalBody>
+        <ModalFooter>
+          <Button
+            color='primary'
+            onClick={deleteWorkstation}
+            disabled={submitting}
+          >
+            Go! {submitting && <i className='fas fa-circle-notch fa-spin' />}
+          </Button>{' '}
+        </ModalFooter>
+      </Modal>
+    </div>
+  );
+};
+
+export default DeleteWorkstation;
