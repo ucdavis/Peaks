9bddfbd4ff184e1b937f646b29194256f6e39b05
Bryan M Wong <bmwong@ucdavis.edu>
2020-09-22 11:20:23 -0700

    Updated KeySerialAssignmentValues and KeySerialEditValues to use hooks

--- Keas.Mvc/ClientApp/components/Keys/KeySerialAssignmentValues.tsx
+++ Keas.Mvc/ClientApp/components/Keys/KeySerialAssignmentValues.tsx
@@ -8,55 +8,49 @@ interface IProps {
   openUpdateModal: (keySerial: IKeySerial) => void;
 }
 
-export default class KeySerialAssignmentValues extends React.Component<
-  IProps,
-  {}
-> {
-  public render() {
-    if (
-      !this.props.selectedKeySerial ||
-      !this.props.selectedKeySerial.keySerialAssignment
-    ) {
-      return null;
-    }
-    return (
-      <div>
-        <div className='row justify-content-between mt-5'>
-          <h3>Assignment Details</h3>
-          <Button
-            color='link'
-            onClick={() =>
-              this.props.openUpdateModal(this.props.selectedKeySerial)
-            }
-          >
-            <i className='fas fa-edit fa-xs' /> Update Assignment
-          </Button>
+const KeySerialAssignmentValues = (props: IProps) => {
+  if (
+    !props.selectedKeySerial ||
+    !props.selectedKeySerial.keySerialAssignment
+  ) {
+    return null;
+  }
+
+  return (
+    <div>
+      <div className='row justify-content-between mt-5'>
+        <h3>Assignment Details</h3>
+        <Button
+          color='link'
+          onClick={() => props.openUpdateModal(props.selectedKeySerial)}
+        >
+          <i className='fas fa-edit fa-xs' /> Update Assignment
+        </Button>
+      </div>
+      <div className='wrapperasset'>
+        <div className='form-group'>
+          <label>Assigned To</label>
+          <input
+            type='text'
+            className='form-control'
+            disabled={true}
+            value={props.selectedKeySerial.keySerialAssignment.person.name}
+          />
         </div>
-        <div className='wrapperasset'>
-          <div className='form-group'>
-            <label>Assigned To</label>
-            <input
-              type='text'
-              className='form-control'
-              disabled={true}
-              value={
-                this.props.selectedKeySerial.keySerialAssignment.person.name
-              }
-            />
-          </div>
-          <div className='form-group'>
-            <label>Expires at</label>
-            <input
-              type='text'
-              className='form-control'
-              disabled={true}
-              value={DateUtil.formatExpiration(
-                this.props.selectedKeySerial.keySerialAssignment.expiresAt
-              )}
-            />
-          </div>
+        <div className='form-group'>
+          <label>Expires at</label>
+          <input
+            type='text'
+            className='form-control'
+            disabled={true}
+            value={DateUtil.formatExpiration(
+              props.selectedKeySerial.keySerialAssignment.expiresAt
+            )}
+          />
         </div>
       </div>
-    );
-  }
-}
+    </div>
+  );
+};
+
+export default KeySerialAssignmentValues;
