ad82408302bbe0e0ecdcbf4f93a4e6bdfe337326
Jason Sylvestre <jsylvestre@ucdavis.edu>
2020-10-30 11:39:26 -0700

    common part of query

--- Keas.Mvc/Resources/PeopleQueries.cs
+++ Keas.Mvc/Resources/PeopleQueries.cs
@@ -29,11 +29,8 @@ from (select People.Id, count(E.Id) as EquipmentCount
 where (People.Active = @active1 or People.Active = @active2) and People.TeamId = @teamId;";
 
 
-
-
-
-
-    public static string PeopleLeavingWithAssets = @"select People.*, SUP.FirstName SupervisorFirstName, SUP.LastName SupervisorLastName, SUP.Email SupervisorEmail, EquipmentCount, AccessCount, KeyCount, WorkstationCount, Teams.Slug
+    private static string PeopleLeavingCommon =
+        @"select People.*, SUP.FirstName SupervisorFirstName, SUP.LastName SupervisorLastName, SUP.Email SupervisorEmail, EquipmentCount, AccessCount, KeyCount, WorkstationCount, Teams.Slug
     from (select People.Id, count(E.Id) as EquipmentCount
       from People
         left join EquipmentAssignments E on People.Id = E.PersonId
@@ -59,35 +56,13 @@ where (People.Active = @active1 or People.Active = @active2) and People.TeamId =
     ) t4 on t1.Id = t4.Id
     left outer join People SUP on People.SupervisorId = SUP.Id
     INNER JOIN
-         Teams ON People.TeamId = dbo.Teams.Id
+         Teams ON People.TeamId = dbo.Teams.Id ";
+
+
+
+    public static string PeopleLeavingWithAssets =  @$"{PeopleLeavingCommon}
 where (People.Active = 0 or (People.EndDate is not null and People.EndDate <= @enddate)) and (EquipmentCount > 0 or AccessCount > 0 or KeyCount > 0 or WorkstationCount > 0) and People.TeamId = @teamId;";
 
-    public static string PeopleLeavingWithAssetsInGroup = @"select People.*, SUP.FirstName SupervisorFirstName, SUP.LastName SupervisorLastName, SUP.Email SupervisorEmail, EquipmentCount, AccessCount, KeyCount, WorkstationCount, Teams.Slug
-    from (select People.Id, count(E.Id) as EquipmentCount
-      from People
-        left join EquipmentAssignments E on People.Id = E.PersonId
-        group by People.Id) t1
-    left outer join (
-        select People.Id, count(Assignment.Id) as AccessCount
-        from People
-            left join AccessAssignments Assignment on People.Id = Assignment.PersonId
-            group by People.Id
-      ) t2 on t1.Id = t2.Id
-    left outer join (
-      select People.Id, count(K.Id) KeyCount
-      from People
-        left join KeySerialAssignments K on People.Id = K.PersonId
-        group by People.Id
-    ) t3 on t1.Id = t3.Id
-    inner join People on People.Id = t1.Id
-    left outer join (
-      select People.Id, count(WA.Id) WorkstationCount
-      from People
-        left join WorkstationAssignments WA on People.Id = WA.PersonId
-        group by People.Id
-    ) t4 on t1.Id = t4.Id
-    left outer join People SUP on People.SupervisorId = SUP.Id
-    INNER JOIN
-         Teams ON People.TeamId = dbo.Teams.Id
+    public static string PeopleLeavingWithAssetsInGroup = @$"{PeopleLeavingCommon}
 where (People.Active = 0 or (People.EndDate is not null and People.EndDate <= @enddate)) and (EquipmentCount > 0 or AccessCount > 0 or KeyCount > 0 or WorkstationCount > 0) and People.TeamId in @teamIds;";
 }
