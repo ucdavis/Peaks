837819d468ef987cfe7466397e4999e46f64de02
Spruce Weber-Milne <swebermilne@ucdavis.edu>
2020-11-17 10:59:17 -0800

    Bump size of Teams.DocumentAccountName

--- Keas.Sql/dbo/Tables/Teams.sql
+++ Keas.Sql/dbo/Tables/Teams.sql
@@ -4,7 +4,7 @@ CREATE TABLE [dbo].[Teams] (
     [Slug]    NVARCHAR (40)    NOT NULL,
     [ApiCode] UNIQUEIDENTIFIER NULL,
     [BoardingNotificationEmail] NVARCHAR(256) NULL, 
-    [DocumentAccountName] NVARCHAR(64) NULL, 
+    [DocumentAccountName] NVARCHAR(256) NULL, 
     [DocumentAccountId] NVARCHAR(64) NULL, 
     [DocumentApiBasePath] NVARCHAR(64) NULL, 
     CONSTRAINT [PK_Teams] PRIMARY KEY CLUSTERED ([Id] ASC)
