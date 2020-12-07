affae41a0a08f2510cbbbb6e84d80bc46ef614e1
Scott Kirkland <srkirkland@gmail.com>
2020-12-07 11:37:40 -0800

    log name change

--- Keas.Mvc/Helpers/LogConfiguration.cs
+++ Keas.Mvc/Helpers/LogConfiguration.cs
@@ -80,7 +80,7 @@ namespace Keas.Mvc.Helpers
             }
             
             return logConfig.WriteTo.Elasticsearch(new ElasticsearchSinkOptions(new Uri(esUrl)) {
-                IndexFormat = $"logs-{loggingSection.GetValue<string>("AppName")}-{loggingSection.GetValue<string>("Environment")}-{{0:yyyy.MM.dd}}",
+                IndexFormat = $"logstash-{loggingSection.GetValue<string>("AppName")}-{loggingSection.GetValue<string>("Environment")}-{{0:yyyy.MM.dd}}",
                 AutoRegisterTemplate = true,
                 AutoRegisterTemplateVersion = AutoRegisterTemplateVersion.ESv7
             });
