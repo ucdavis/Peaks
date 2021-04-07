bf4fe9e76fb74f37e38c42284899c084091505a8
Bryan M Wong <bmwong@ucdavis.edu>
2021-04-07 11:48:40 -0700

    Fixed webpack error

--- Keas.Mvc/ClientApp/components/Access/AccessAssignmentContainer.tsx
+++ Keas.Mvc/ClientApp/components/Access/AccessAssignmentContainer.tsx
@@ -177,7 +177,7 @@ const AssignmentContainer = (props: IProps) => {
     const assignmentsData = assignments;
     assignments.push(accessAssignment);
     setAssignments(assignmentsData);
-    props.onAssignSuccess(accessAssignment);
+    props.onAssignSuccess();
   };
 
   const openDetails = (access: IAccess) => {
