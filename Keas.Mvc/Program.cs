e9dee02492764aad3c5635b8457d6cd96e84a825
Spruce Weber-Milne <swebermilne@ucdavis.edu>
2022-01-13 10:04:54 -0800

    Renable APM enrichment

--- Keas.Mvc/Program.cs
+++ Keas.Mvc/Program.cs
@@ -89,7 +89,7 @@ namespace Keas.Mvc
                 .MinimumLevel.Override("Microsoft", LogEventLevel.Information)
                 // .MinimumLevel.Override("Microsoft.EntityFrameworkCore", LogEventLevel.Warning) // uncomment this to hide EF core general info logs
                 .MinimumLevel.Override("Microsoft.AspNetCore", LogEventLevel.Warning)
-                //.Enrich.WithElasticApmCorrelationInfo()
+                .Enrich.WithElasticApmCorrelationInfo()
                 .Enrich.FromLogContext()
                 .Enrich.WithClientIp()
                 .Enrich.WithClientAgent()
