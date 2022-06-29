656c7cbbe2ee5189b74cf861c37e4c3ec30bcb50
Jason Sylvestre <jsylvestre@ucdavis.edu>
2022-06-29 13:23:02 -0700

    Completed doc report

---
+++ Keas.Mvc/Models/ReportModels/CompletedDocsReportModel.cs
@@ -0,0 +1,21 @@
+using Keas.Core.Domain;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+
+namespace Keas.Mvc.Models.ReportModels
+{
+    public class CompletedDocsReportModel
+    {
+        [DataType(DataType.Date)]
+        [DisplayFormat(DataFormatString = "{0:yyyy-MM-dd}", ApplyFormatInEditMode = true)]
+        [Display(Name = "Start Created Date")]
+        public DateTime Start {get;set; }
+
+        [DataType(DataType.Date)]
+        [DisplayFormat(DataFormatString = "{0:yyyy-MM-dd}", ApplyFormatInEditMode = true)]
+        [Display(Name = "End Created Date")]
+        public DateTime End { get; set; }
+        public List<Document> docs {get;set;}
+    }
+}
