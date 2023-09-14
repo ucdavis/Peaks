195ba97bdd2ccd6bd5c1fa31f838a6453c615f55
Jason Sylvestre <jsylvestre@ucdavis.edu>
2023-09-14 11:16:51 -0700

    Add and refactor completed documents report

--- Keas.Mvc/Models/ReportModels/CompletedDocsReportModel.cs
+++ Keas.Mvc/Models/ReportModels/CompletedDocsReportModel.cs
@@ -16,6 +16,24 @@ namespace Keas.Mvc.Models.ReportModels
         [DisplayFormat(DataFormatString = "{0:yyyy-MM-dd}", ApplyFormatInEditMode = true)]
         [Display(Name = "End Created Date")]
         public DateTime End { get; set; }
+
+        public Group Group { get; set; }
         public List<Document> Docs {get;set;}
+
+        public CompletedDocsReportModelItem[] Items { get; set; }
+    }
+
+    public class CompletedDocsReportModelItem
+    {
+        public string PersonName { get; set; }
+        public string DocName { get; set; }
+        [DisplayFormat(DataFormatString = "{0:d}")]
+        public DateTime CreatedAt { get; set; }
+        [DisplayFormat(DataFormatString = "{0:d}")]
+        public DateTime? CompletedAt { get; set; }
+        public string TeamSlug { get; set; }
+        public string TeamName { get; set; }
+        public string DetailsLink { get; set; }
+
     }
 }
