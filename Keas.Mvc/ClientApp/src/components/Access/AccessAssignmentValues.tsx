7a78aa32be7dfe73619fd20a66e85c4017c6e56e
River Holstege <laholstege@ucdavis.edu>
2023-09-20 10:22:48 -0700

    add AccessAssignmentEditValues

---
+++ Keas.Mvc/ClientApp/src/components/Access/AccessAssignmentValues.tsx
@@ -0,0 +1,64 @@
+﻿import * as React from 'react';
+import { Button } from 'reactstrap';
+import { DateUtil } from '../../util/dates';
+import { IAccessAssignment } from '../../models/Access';
+
+interface IProps {
+  selectedAccessAssignment: IAccessAssignment;
+  openUpdateModal?: (accessAssignment: IAccessAssignment) => void;
+}
+
+const AccessAssignmentEditValues = (props: IProps) => {
+  if (!props.selectedAccessAssignment) {
+    return null;
+  }
+
+  return (
+    <div>
+      <div className='row justify-content-between mt-5'>
+        <h3>Assignment Details</h3>
+        <Button
+          color='link'
+          onClick={() => props.openUpdateModal(props.selectedAccessAssignment)}
+        >
+          <i className='fas fa-edit fa-xs' />{' '}
+          {!!props.selectedAccessAssignment
+            ? 'Update Assignment'
+            : 'Assign Equipment'}
+        </Button>
+      </div>
+      <div className='wrapperasset'>
+        <div className='form-group'>
+          <label>Assigned To</label>
+          <input
+            type='text'
+            className='form-control'
+            disabled={true}
+            value={
+              !!props.selectedAccessAssignment
+                ? props.selectedAccessAssignment.person.name
+                : ''
+            }
+          />
+        </div>
+        <div className='form-group'>
+          <label>Expires at</label>
+          <input
+            type='text'
+            className='form-control'
+            disabled={true}
+            value={
+              !!props.selectedAccessAssignment
+                ? DateUtil.formatExpiration(
+                    props.selectedAccessAssignment.expiresAt
+                  )
+                : ''
+            }
+          />
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default AccessAssignmentEditValues;
